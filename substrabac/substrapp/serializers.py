06ea29c6d39096058ccce51fc6536d26c1b7d675
Camille Marini <camille.marini@gmail.com>
2018-05-16 17:55:25 +0200

    fix pb serializer

--- substrabac/substrapp/serializers.py
+++ substrabac/substrapp/serializers.py
@@ -3,19 +3,15 @@ from .models import Problem, DataOpener, Data
 
 
 class ProblemSerializer(serializers.ModelSerializer):
-    test_data = serializers.ListField(child=serializers.CharField(
-        min_length=69, max_length=69), min_length=1, max_length=None)
-    name = serializers.CharField(min_length=1, max_length=60)
 
     class Meta:
         model = Problem
         fields = '__all__'
 
-    # def create(self, validated_data):
-    #     Problem.objects.create(description=validated_data["description"],
-    #                            metrics=validated_data["metrics"])
-    #     
 
-    # def update(self, instance, validated_data):
+class LedgerProblemSerializer(serializers.Serializer):
+    test_data = serializers.ListField(child=serializers.CharField(
+        min_length=69, max_length=69), min_length=1, max_length=None)
+    name = serializers.CharField(min_length=1, max_length=60)
 
     
