31e56cb985d24c1edd37fbe116f28004c08c3f46
GuillaumeCisco <guillaumecisco@gmail.com>
2018-05-17 11:40:37 +0200

    Update settings for maintaining dev and prod

---
+++ substrabac/substrabac/settings/dev.py
@@ -0,0 +1,60 @@
+import os
+
+from .common import *
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('SUBSTRABAC_DB_NAME', 'substrabac'),
+        'USER': os.environ.get('SUBSTRABAC_DB_USER', 'substrabac'),
+        'PASSWORD': os.environ.get('SUBSTRABAC_DB_PWD', 'substrabac'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': 5432,
+    }
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'error_file': {
+            'level': 'INFO',
+            'filename': os.path.join(PROJECT_ROOT, 'substrabac.log'),
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1 * 1024 * 1024,
+            'backupCount': 2,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'error_file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
