62e7bbac45513806c2ea9076e1eacd2f2da77ca5
GuillaumeCisco <guillaumecisco@gmail.com>
2018-06-06 16:07:17 +0200

    Add Header versioning and tests

--- substrabac/substrabac/settings/common.py
+++ substrabac/substrabac/settings/common.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'libs.SQLPrintingMiddleware.SQLPrintingMiddleware',
+    'libs.SQLPrintingMiddleware.SQLPrintingMiddleware',
 ]
 
 ROOT_URLCONF = 'substrabac.urls'
