1f321886424a5dd2d471f14a961c8156f5f1d59f
GuillaumeCisco <guillaumecisco@gmail.com>
2018-06-08 13:05:59 +0200

    Add problem creation logic

--- substrabac/substrapp/serializers/ledger/problem.py
+++ substrabac/substrapp/serializers/ledger/problem.py
@@ -1,4 +1,8 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+
+from substrapp.conf import conf
+from substrapp.models import Problem
+from substrapp.views.utils import invokeLedger
 
 
 class LedgerProblemSerializer(serializers.Serializer):
@@ -9,7 +13,22 @@ class LedgerProblemSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         instance_pkhash = self.initial_data.get('instance_pkhash')
+        test_data = self.initial_data.get('test_data')
 
         # TODO use asynchrone task for calling ledger
+        org = conf['orgs']['chu-nantes']
+        peer = org['peers'][0]
+        options = {
+            'org': org,
+            'peer': peer,
+            'args': '{"Args":["registerProblem", "'+ instance_pkhash + '", "' + str(len(test_data)) + '", "' + ','.join([x for x in test_data]) + '"]}'
+        }
+        data, st = invokeLedger(options)
 
+        # TODO : remove when using celery tasks
+        #  if not created on ledger, delete from local db
+        if st != status.HTTP_201_CREATED:
+            Problem.objects.get(pk=instance_pkhash).delete()
         # pass instance to validated True if ok, else create cron that delete instance.validated = False older than x days
+
+        return data, st
