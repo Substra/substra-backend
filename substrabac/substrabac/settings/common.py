93b3451945f55248b8ba6be7ddb5dd6dd3c50986
GuillaumeCisco <guillaumecisco@gmail.com>
2018-06-08 13:32:11 +0200

    Add swagger config

--- substrabac/substrabac/settings/common.py
+++ substrabac/substrabac/settings/common.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
     'rest_framework',
     'substrapp',
 ]
