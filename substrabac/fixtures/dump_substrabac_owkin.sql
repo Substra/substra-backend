8b235e1e890b809c6b1c8f6d3436e291f12d6dd3
GuillaumeCisco <guillaumecisco@gmail.com>
2018-09-11 15:50:45 +0200

    Revert "Update file in model null"
    This reverts commit 80b63d15a99cab048efeddb11289309c5520d98a.

--- substrabac/fixtures/dump_substrabac_owkin.sql
+++ substrabac/fixtures/dump_substrabac_owkin.sql
@@ -476,7 +476,7 @@ ALTER TABLE public.substrapp_dataset OWNER TO substrabac;
 CREATE TABLE public.substrapp_model (
     pkhash character varying(64) NOT NULL,
     validated boolean NOT NULL,
-    file character varying(500)
+    file character varying(500) NOT NULL
 );
 
 
@@ -576,50 +576,50 @@ COPY public.auth_permission (id, name, content_type_id, codename) FROM stdin;
 1	Can add log entry	1	add_logentry
 2	Can change log entry	1	change_logentry
 3	Can delete log entry	1	delete_logentry
-4	Can view log entry	1	view_logentry
-5	Can add permission	2	add_permission
-6	Can change permission	2	change_permission
-7	Can delete permission	2	delete_permission
-8	Can view permission	2	view_permission
-9	Can add group	3	add_group
-10	Can change group	3	change_group
-11	Can delete group	3	delete_group
-12	Can view group	3	view_group
-13	Can add user	4	add_user
-14	Can change user	4	change_user
-15	Can delete user	4	delete_user
-16	Can view user	4	view_user
-17	Can add content type	5	add_contenttype
-18	Can change content type	5	change_contenttype
-19	Can delete content type	5	delete_contenttype
-20	Can view content type	5	view_contenttype
-21	Can add session	6	add_session
-22	Can change session	6	change_session
-23	Can delete session	6	delete_session
-24	Can view session	6	view_session
-25	Can add site	7	add_site
-26	Can change site	7	change_site
-27	Can delete site	7	delete_site
-28	Can view site	7	view_site
-29	Can add algo	8	add_algo
-30	Can change algo	8	change_algo
-31	Can delete algo	8	delete_algo
-32	Can view algo	8	view_algo
-33	Can add challenge	9	add_challenge
-34	Can change challenge	9	change_challenge
-35	Can delete challenge	9	delete_challenge
-36	Can view challenge	9	view_challenge
-37	Can add data	10	add_data
-38	Can change data	10	change_data
-39	Can delete data	10	delete_data
-40	Can view data	10	view_data
-41	Can add dataset	11	add_dataset
-42	Can change dataset	11	change_dataset
-43	Can delete dataset	11	delete_dataset
-44	Can view dataset	11	view_dataset
-45	Can add model	12	add_model
-46	Can change model	12	change_model
-47	Can delete model	12	delete_model
+4	Can add permission	2	add_permission
+5	Can change permission	2	change_permission
+6	Can delete permission	2	delete_permission
+7	Can add group	3	add_group
+8	Can change group	3	change_group
+9	Can delete group	3	delete_group
+10	Can add user	4	add_user
+11	Can change user	4	change_user
+12	Can delete user	4	delete_user
+13	Can add content type	5	add_contenttype
+14	Can change content type	5	change_contenttype
+15	Can delete content type	5	delete_contenttype
+16	Can add session	6	add_session
+17	Can change session	6	change_session
+18	Can delete session	6	delete_session
+19	Can add site	7	add_site
+20	Can change site	7	change_site
+21	Can delete site	7	delete_site
+22	Can add algo	8	add_algo
+23	Can change algo	8	change_algo
+24	Can delete algo	8	delete_algo
+25	Can add challenge	9	add_challenge
+26	Can change challenge	9	change_challenge
+27	Can delete challenge	9	delete_challenge
+28	Can add data	10	add_data
+29	Can change data	10	change_data
+30	Can delete data	10	delete_data
+31	Can add dataset	11	add_dataset
+32	Can change dataset	11	change_dataset
+33	Can delete dataset	11	delete_dataset
+34	Can add model	12	add_model
+35	Can change model	12	change_model
+36	Can delete model	12	delete_model
+37	Can view log entry	1	view_logentry
+38	Can view permission	2	view_permission
+39	Can view group	3	view_group
+40	Can view user	4	view_user
+41	Can view content type	5	view_contenttype
+42	Can view session	6	view_session
+43	Can view site	7	view_site
+44	Can view algo	8	view_algo
+45	Can view challenge	9	view_challenge
+46	Can view data	10	view_data
+47	Can view dataset	11	view_dataset
 48	Can view model	12	view_model
 \.
 
@@ -681,27 +681,26 @@ COPY public.django_content_type (id, app_label, model) FROM stdin;
 --
 
 COPY public.django_migrations (id, app, name, applied) FROM stdin;
-1	contenttypes	0001_initial	2018-09-11 15:34:21.787981+02
-2	auth	0001_initial	2018-09-11 15:34:22.034401+02
-3	admin	0001_initial	2018-09-11 15:34:22.121615+02
-4	admin	0002_logentry_remove_auto_add	2018-09-11 15:34:22.143738+02
-5	admin	0003_logentry_add_action_flag_choices	2018-09-11 15:34:22.161111+02
-6	contenttypes	0002_remove_content_type_name	2018-09-11 15:34:22.194851+02
-7	auth	0002_alter_permission_name_max_length	2018-09-11 15:34:22.203453+02
-8	auth	0003_alter_user_email_max_length	2018-09-11 15:34:22.216211+02
-9	auth	0004_alter_user_username_opts	2018-09-11 15:34:22.227994+02
-10	auth	0005_alter_user_last_login_null	2018-09-11 15:34:22.242839+02
-11	auth	0006_require_contenttypes_0002	2018-09-11 15:34:22.246045+02
-12	auth	0007_alter_validators_add_error_messages	2018-09-11 15:34:22.256804+02
-13	auth	0008_alter_user_username_max_length	2018-09-11 15:34:22.28441+02
-14	auth	0009_alter_user_last_name_max_length	2018-09-11 15:34:22.299089+02
-15	sessions	0001_initial	2018-09-11 15:34:22.348487+02
-16	sites	0001_initial	2018-09-11 15:34:22.375387+02
-17	sites	0002_alter_domain_unique	2018-09-11 15:34:22.408711+02
-18	substrapp	0001_initial	2018-09-11 15:34:22.54482+02
-19	substrapp	0002_auto_20180821_1615	2018-09-11 15:34:22.670002+02
-20	substrapp	0003_auto_20180831_1339	2018-09-11 15:34:22.7022+02
-21	substrapp	0004_auto_20180911_1334	2018-09-11 15:34:22.711279+02
+1	contenttypes	0001_initial	2018-08-29 18:05:11.121702+02
+2	auth	0001_initial	2018-08-29 18:05:11.339827+02
+3	admin	0001_initial	2018-08-29 18:05:11.409992+02
+4	admin	0002_logentry_remove_auto_add	2018-08-29 18:05:11.441996+02
+5	contenttypes	0002_remove_content_type_name	2018-08-29 18:05:11.468696+02
+6	auth	0002_alter_permission_name_max_length	2018-08-29 18:05:11.476193+02
+7	auth	0003_alter_user_email_max_length	2018-08-29 18:05:11.488054+02
+8	auth	0004_alter_user_username_opts	2018-08-29 18:05:11.4971+02
+9	auth	0005_alter_user_last_login_null	2018-08-29 18:05:11.509746+02
+10	auth	0006_require_contenttypes_0002	2018-08-29 18:05:11.513954+02
+11	auth	0007_alter_validators_add_error_messages	2018-08-29 18:05:11.540434+02
+12	auth	0008_alter_user_username_max_length	2018-08-29 18:05:11.561164+02
+13	auth	0009_alter_user_last_name_max_length	2018-08-29 18:05:11.572269+02
+14	sessions	0001_initial	2018-08-29 18:05:11.605552+02
+15	sites	0001_initial	2018-08-29 18:05:11.629132+02
+16	sites	0002_alter_domain_unique	2018-08-29 18:05:11.657023+02
+17	substrapp	0001_initial	2018-08-29 18:05:11.761932+02
+18	substrapp	0002_auto_20180821_1615	2018-08-29 18:05:11.819214+02
+19	admin	0003_logentry_add_action_flag_choices	2018-08-31 15:39:55.425049+02
+20	substrapp	0003_auto_20180831_1339	2018-08-31 15:39:55.439014+02
 \.
 
 
@@ -744,8 +743,8 @@ COPY public.substrapp_challenge (creation_date, last_modified, pkhash, validated
 --
 
 COPY public.substrapp_data (pkhash, validated, file) FROM stdin;
-4b5152871b181d10ee774c10458c064c70710f4ba35938f10c0b7aa51f7dc010	t	data/4b5152871b181d10ee774c10458c064c70710f4ba35938f10c0b7aa51f7dc010/0024701.zip
 e11aeec290749e4c50c91305e10463eced8dbf3808971ec0c6ea0e36cb7ab3e1	t	data/e11aeec290749e4c50c91305e10463eced8dbf3808971ec0c6ea0e36cb7ab3e1/0024900.zip
+4b5152871b181d10ee774c10458c064c70710f4ba35938f10c0b7aa51f7dc010	t	data/4b5152871b181d10ee774c10458c064c70710f4ba35938f10c0b7aa51f7dc010/0024701.zip
 \.
 
 
@@ -826,7 +825,7 @@ SELECT pg_catalog.setval('public.django_content_type_id_seq', 12, true);
 -- Name: django_migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: substrabac
 --
 
-SELECT pg_catalog.setval('public.django_migrations_id_seq', 21, true);
+SELECT pg_catalog.setval('public.django_migrations_id_seq', 20, true);
 
 
 --
