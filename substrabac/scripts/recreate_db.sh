514d2d039a54bd2a961ab4a3e069d67fe3558dc1
Kelvin Moutet <kelvin.moutet@owkin.com>
2018-09-13 18:12:38 +0200

    Update postgresql script to use -U (user) option.

--- substrabac/scripts/recreate_db.sh
+++ substrabac/scripts/recreate_db.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-dropdb substrabac_owkin
-createdb -E UTF8 substrabac_owkin
-psql -d substrabac_owkin -c "GRANT ALL PRIVILEGES ON DATABASE substrabac_owkin to substrabac;ALTER ROLE substrabac WITH SUPERUSER CREATEROLE CREATEDB;"
+dropdb -U ${USER} substrabac
+createdb -U ${USER} -E UTF8 substrabac
+psql -U ${USER} -d substrabac -c "CREATE USER substrabac WITH PASSWORD 'substrabac' CREATEDB CREATEROLE SUPERUSER;"
 
-dropdb substrabac_chunantes
-createdb -E UTF8 substrabac_chunantes
-psql -d substrabac_chunantes -c "GRANT ALL PRIVILEGES ON DATABASE substrabac_chunantes to substrabac;ALTER ROLE substrabac WITH SUPERUSER CREATEROLE CREATEDB;"
+dropdb -U ${USER} substrabac_owkin
+createdb -U ${USER} -E UTF8 substrabac_owkin
+psql -U ${USER} -d substrabac_owkin -c "GRANT ALL PRIVILEGES ON DATABASE substrabac_owkin to substrabac;ALTER ROLE substrabac WITH SUPERUSER CREATEROLE CREATEDB;"
+
+dropdb -U ${USER} substrabac_chunantes
+createdb -U ${USER} -E UTF8 substrabac_chunantes
+psql -U ${USER} -d substrabac_chunantes -c "GRANT ALL PRIVILEGES ON DATABASE substrabac_chunantes to substrabac;ALTER ROLE substrabac WITH SUPERUSER CREATEROLE CREATEDB;"
