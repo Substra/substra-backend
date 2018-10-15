4ae830760b599f8a0ceac821330ffd9e5fb4c533
Kelvin Moutet <kelvin.moutet@owkin.com>
2018-10-15 12:06:45 +0200

    Update fixture for docker.

--- substrabac/substrapp/fixtures/model.py
+++ substrabac/substrapp/fixtures/model.py
@@ -3,26 +3,26 @@ fake_models = [
         'algo': {
             'hash': '76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895',
             'name': 'Neural Network',
-            'storageAddress': 'http://127.0.0.1:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
         },
         'challenge': {
             'hash': 'd5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f',
             'metrics': {
                 'hash': '750f622262854341bd44f55c1018949e9c119606ef5068bd7d137040a482a756',
-                'storageAddress': 'http://127.0.0.1:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
+                'storageAddress': 'http://chunantes.substrabac:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
             },
         },
         'creator': 'a3119c79a173581425cbe6e06c3034ec396ee805b60d9a34feaa3048beb0e4a9',
         'endModel': {
             'hash': '30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'key': '1bb5c8f42315914909c764545ea44e32b04c773468c439c9eb506176670ee6b8',
         'log': 'no error, ah ah ahstill no error, suprah ah ah',
         'permissions': 'all',
         'startModel': {
             'hash': '20060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/20060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/20060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'status': 'done',
         'testData': {
@@ -43,26 +43,26 @@ fake_models = [
         'algo': {
             'hash': '76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895',
             'name': 'Neural Network',
-            'storageAddress': 'http://127.0.0.1:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
         },
         'challenge': {
             'hash': 'd5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f',
             'metrics': {
                 'hash': '750f622262854341bd44f55c1018949e9c119606ef5068bd7d137040a482a756',
-                'storageAddress': 'http://127.0.0.1:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
+                'storageAddress': 'http://chunantes.substrabac:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
             },
         },
         'creator': 'a3119c79a173581425cbe6e06c3034ec396ee805b60d9a34feaa3048beb0e4a9',
         'endModel': {
             'hash': '40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'key': '2bb5c8f42315914909c764545ea44e32b04c773468c439c9eb506176670ee6b8',
         'log': 'no error, ah ah ahstill no error, suprah ah ah',
         'permissions': 'all',
         'startModel': {
             'hash': '30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/30060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'status': 'done',
         'testData': {
@@ -83,26 +83,26 @@ fake_models = [
         'algo': {
             'hash': '76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895',
             'name': 'Neural Network',
-            'storageAddress': 'http://127.0.0.1:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/algo/76fe474d441b03e8416ab37b4950286014fb329e9317126e144342dd0e2ec895/file/',
         },
         'challenge': {
             'hash': 'd5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f',
             'metrics': {
                 'hash': '750f622262854341bd44f55c1018949e9c119606ef5068bd7d137040a482a756',
-                'storageAddress': 'http://127.0.0.1:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
+                'storageAddress': 'http://chunantes.substrabac:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
             },
         },
         'creator': 'a3119c79a173581425cbe6e06c3034ec396ee805b60d9a34feaa3048beb0e4a9',
         'endModel': {
             'hash': '50060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/50060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/50060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'key': '3bb5c8f42315914909c764545ea44e32b04c773468c439c9eb506176670ee6b8',
         'log': 'no error, ah ah ahstill no error, suprah ah ah',
         'permissions': 'all',
         'startModel': {
             'hash': '40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/40060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'status': 'done',
         'testData': {
@@ -123,26 +123,26 @@ fake_models = [
         'algo': {
             'hash': '56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df',
             'name': 'Random Forest',
-            'storageAddress': 'http://127.0.0.1:8001/56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df/file/',
         },
         'challenge': {
             'hash': 'd5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f',
             'metrics': {
                 'hash': '750f622262854341bd44f55c1018949e9c119606ef5068bd7d137040a482a756',
-                'storageAddress': 'http://127.0.0.1:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
+                'storageAddress': 'http://chunantes.substrabac:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
             },
         },
         'creator': 'a3119c79a173581425cbe6e06c3034ec396ee805b60d9a34feaa3048beb0e4a9',
         'endModel': {
             'hash': '70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'key': '4bb5c8f42315914909c764545ea44e32b04c773468c439c9eb506176670ee6b8',
         'log': 'no error, ah ah ahstill no error, suprah ah ah',
         'permissions': 'all',
         'startModel': {
             'hash': '60060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/60060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/60060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'status': 'done',
         'testData': {
@@ -163,26 +163,26 @@ fake_models = [
         'algo': {
             'hash': '56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df',
             'name': 'Random Forest',
-            'storageAddress': 'http://127.0.0.1:8001/algo/56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/algo/56a0e2f7e046ee948cf2ab38136f7b5ff131d0c538f8d75a97850d6fc06131df/file/',
         },
         'challenge': {
             'hash': 'd5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f',
             'metrics': {
                 'hash': '750f622262854341bd44f55c1018949e9c119606ef5068bd7d137040a482a756',
-                'storageAddress': 'http://127.0.0.1:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
+                'storageAddress': 'http://chunantes.substrabac:8001/challenge/d5002e1cd50bd5de5341df8a7b7d11b6437154b3b08f531c9b8f93889855c66f/metrics/',
             },
         },
         'creator': 'a3119c79a173581425cbe6e06c3034ec396ee805b60d9a34feaa3048beb0e4a9',
         'endModel': {
             'hash': '80060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/80060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/80060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'key': '5bb5c8f42315914909c764545ea44e32b04c773468c439c9eb506176670ee6b8',
         'log': 'no error, ah ah ahstill no error, suprah ah ah',
         'permissions': 'all',
         'startModel': {
             'hash': '70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://127.0.0.1:8001/model/70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
+            'storageAddress': 'http://chunantes.substrabac:8001/model/70060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
         },
         'status': 'done',
         'testData': {
