83c5886a4a578bbe7d0578be9075e7ea82f4bb37
GuillaumeCisco <guillaumecisco@gmail.com>
2018-11-09 15:17:28 +0100

    update static in prod

--- substrabac/substrabac/settings/prod/chunantes.py
+++ substrabac/substrabac/settings/prod/chunantes.py
@@ -30,6 +30,9 @@ DATABASES = {
 
 MEDIA_ROOT = '/substra/medias/chunantes'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'statics')
+
 SITE_ID = 1
 SITE_HOST = os.environ.get('SITE_HOST', 'chunantes.substrabac')
 SITE_PORT = os.environ.get('SITE_PORT', '8001')
--- substrabac/substrabac/settings/prod/owkin.py
+++ substrabac/substrabac/settings/prod/owkin.py
@@ -30,6 +30,9 @@ DATABASES = {
 
 MEDIA_ROOT = '/substra/medias/owkin'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'statics')
+
 SITE_ID = 1
 SITE_HOST = os.environ.get('SITE_HOST', 'owkin.substrabac')
 SITE_PORT = os.environ.get('SITE_PORT', '8000')
