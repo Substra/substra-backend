07d5c449dcddd80a9420ddf5c243cd1ba5a79845
GuillaumeCisco <guillaumecisco@gmail.com>
2018-11-09 14:19:20 +0100

    fix populate_owkin

--- substrabac/populate_owkin.py
+++ substrabac/populate_owkin.py
@@ -14,7 +14,7 @@ except:
 else:
 
     print('Init config in /tmp/.substrabac for owkin and chunantes')
-    res = popen(['substra', 'config', 'https://35.187.120.182:9000', '0.0', getattr(settings, 'BASICAUTH_USERNAME'), getattr(settings, 'BASICAUTH_USERNAME'), '--profile=owkin',
+    res = popen(['substra', 'config', 'https://35.187.120.182:9000', '0.0', getattr(settings, 'BASICAUTH_USERNAME'), getattr(settings, 'BASICAUTH_PASSWORD'), '--profile=owkin',
                  '--config=/tmp/.substrabac'], stdout=PIPE).communicate()[0]
 
     print('create dataset with owkin org')
