84aacdf8adb8792200f92b18913e4541f4ba09bb
GuillaumeCisco <guillaumecisco@gmail.com>
2018-11-16 13:58:25 +0100

    Fix in memory file

--- substrabac/substrapp/models/data.py
+++ substrabac/substrapp/models/data.py
@@ -4,6 +4,7 @@ import zipfile
 from os import path, rename
 
 from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
 from substrapp.utils import get_hash
@@ -34,28 +35,28 @@ class Data(models.Model):
 
 
 @receiver(pre_save, sender=Data)
-def my_handler(sender, instance, **kwargs):
+def data_pre_save(sender, instance, **kwargs):
 
     directory = path.join(getattr(settings, 'MEDIA_ROOT'), 'data/{0}'.format(instance.pk))
 
     # unzip file
-    if path.isfile(instance.path):
+    if isinstance(instance.path, InMemoryUploadedFile):
         try:
             zip_ref = zipfile.ZipFile(instance.path)
             zip_ref.extractall(directory)
             zip_ref.close()
         except Exception as e:
             logging.info(e)
+        else:
+            # calculate new hash
+            sha256hash = dirhash(directory, 'sha256')
+            # rename directory to new hash if does not exist
+            new_directory = 'data/{0}'.format(sha256hash)
+            try:
+                rename(directory, path.join(getattr(settings, 'MEDIA_ROOT'), new_directory))
+            except Exception as e:
+                shutil.rmtree(directory)
+                logging.error(e, exc_info=True)
 
-        # calculate new hash
-        sha256hash = dirhash(directory, 'sha256')
-        # rename directory to new hash if does not exist
-        new_directory = 'data/{0}'.format(sha256hash)
-        try:
-            rename(directory, path.join(getattr(settings, 'MEDIA_ROOT'), new_directory))
-        except Exception as e:
-            shutil.rmtree(directory)
-            logging.error(e, exc_info=True)
-
-        instance.pkhash = sha256hash
-        instance.path = new_directory
+            instance.pkhash = sha256hash
+            instance.path = new_directory
