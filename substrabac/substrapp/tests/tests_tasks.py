6448d5a5e322420decf19b7a3665265e5a81947f
Kelvin Moutet <kelvin.moutet@owkin.com>
2018-11-23 14:19:27 +0100

    Add more tests.

--- substrabac/substrapp/tests/tests_tasks.py
+++ substrabac/substrapp/tests/tests_tasks.py
@@ -12,6 +12,7 @@ from rest_framework.test import APITestCase
 
 from substrapp.utils import compute_hash, get_computed_hash, get_remote_file, untar_algo, get_hash, create_directory
 from substrapp.job_utils import RessourceManager, monitoring_job, compute_docker
+from substrapp.tasks import build_traintuple_folders, get_algo, get_model, get_challenge, put_opener, put_model, put_algo, put_metric, put_data
 
 from .common import get_sample_challenge, get_sample_dataset, get_sample_data, get_sample_script
 
@@ -120,26 +121,133 @@ class TasksTests(APITestCase):
         self.assertNotEqual(monitoring._stats['cpu']['max'], 0)
         self.assertNotEqual(monitoring._stats['netio']['rx'], 0)
 
-    def test_untar_algo(self):
+    def test_put_algo(self):
 
         file = open(os.path.join(MEDIA_ROOT, 'sample_metrics.py'), 'w')
         file.write('Hello World')
         file.close()
-        self.assertTrue(os.path.join(MEDIA_ROOT, 'sample_metrics.py'))
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'sample_metrics.py')))
 
         tf = tarfile.open(os.path.join(MEDIA_ROOT, 'sample.tar.gz'), mode='w:gz')
         tf.add(os.path.join(MEDIA_ROOT, 'sample_metrics.py'), arcname='sample_metrics.py')
         tf.close()
-        self.assertTrue(os.path.join(MEDIA_ROOT, 'sample.tar.gz'))
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'sample.tar.gz')))
 
         with mock.patch('substrapp.tasks.get_hash') as mocked_function:
             with open(os.path.join(MEDIA_ROOT, 'sample.tar.gz'), 'rb') as content:
                 pkhash = get_hash(os.path.join(MEDIA_ROOT, 'sample.tar.gz'))
                 mocked_function.return_value = pkhash
-                untar_algo(content.read(), os.path.join(MEDIA_ROOT, 'traintuple/testkey/'), {'key': 'testkey', 'algo': 'testalgo'})
+                put_algo({'key': 'testkey', 'algo': 'testalgo'}, os.path.join(MEDIA_ROOT, 'traintuple/testkey/'), content.read())
 
         metric_path = os.path.join(MEDIA_ROOT, 'traintuple/testkey/sample_metrics.py')
         self.assertTrue(os.path.exists(metric_path))
+
+    def test_put_metric(self):
+
+        filepath = os.path.join(MEDIA_ROOT, 'sample_metrics.py')
+
+        class FakeMetrics(object):
+            def __init__(self):
+                self.path = filepath
+
+        class FakeChallenge(object):
+            def __init__(self):
+                self.metrics = FakeMetrics()
+
+        file = open(filepath, 'w')
+        file.write('Hello World')
+        file.close()
+        self.assertTrue(os.path.exists(filepath))
+
+        create_directory(os.path.join(MEDIA_ROOT, 'metrics/'))
+        put_metric(MEDIA_ROOT, FakeChallenge())
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'metrics/metrics.py')))
+
+    def test_put_opener(self):
+
+        filepath = os.path.join(MEDIA_ROOT, 'opener.py')
+
+        class FakeOpener(object):
+            def __init__(self):
+                self.path = filepath
+
+        class FakeDataset(object):
+            def __init__(self):
+                self.data_opener = FakeOpener()
+
+        file = open(filepath, 'w')
+        file.write('Hello World')
+        file.close()
+        self.assertTrue(os.path.exists(filepath))
+
+        openerHash = get_hash(filepath)
+        data_type = 'trainData'
+        traintuple = {data_type: {'openerHash': openerHash}}
+
+        create_directory(os.path.join(MEDIA_ROOT, 'opener'))
+        with mock.patch('substrapp.models.Dataset.objects.get') as mocked_get_method:
+            mocked_get_method.return_value = FakeDataset()
+            with self.assertRaises(Exception):
+                put_opener({data_type: {'openerHash': 'HASH'}}, MEDIA_ROOT, data_type)
+
+            put_opener(traintuple, MEDIA_ROOT, data_type)
+
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'opener/opener.py')))
+
+    def test_put_data(self):
+        pass
+
+    def test_put_model(self):
+
+        class FakePath(object):
+            def __init__(self):
+                self.path = os.path.join(MEDIA_ROOT, 'model/model2')
+
+        class FakeModel(object):
+            def __init__(self):
+                self.file = FakePath()
+
+        model_content = b'MODEL 1 2 3'
+        model_hash = compute_hash(model_content)
+        model_type = 'startModel'
+        traintuple = {model_type: {'hash': model_hash}}
+        create_directory(os.path.join(MEDIA_ROOT, 'model/'))
+        put_model(traintuple, MEDIA_ROOT, model_content, model_type)
+
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'model/model')))
+        os.rename(os.path.join(MEDIA_ROOT, 'model/model'), os.path.join(MEDIA_ROOT, 'model/model2'))
+        with mock.patch('substrapp.models.Model.objects.get') as mocked_get_method:
+            mocked_get_method.return_value = FakeModel()
+            put_model(traintuple, MEDIA_ROOT, model_content, model_type)
+
+        self.assertTrue(os.path.exists(os.path.join(MEDIA_ROOT, 'model/model')))
+
+        with mock.patch('substrapp.models.Model.objects.get') as mocked_get_method:
+            mocked_get_method.return_value = FakeModel()
+            with self.assertRaises(Exception):
+                put_model({'startModel': {'hash': 'hash'}}, MEDIA_ROOT, model_content, model_type)
+
+    def test_get_model(self):
+        model_content = b'MODEL 1 2 3'
+        model_hash = compute_hash(model_content)
+        model_type = 'startModel'
+        traintuple = {model_type: {'hash': model_hash}}
+        with mock.patch('substrapp.tasks.get_remote_file') as mocked_get_function:
+            mocked_get_function.return_value = model_content, model_hash
+            model_content, model_hash = get_model(traintuple, model_type)
+
+        self.assertIsNotNone(model_content)
+        self.assertIsNotNone(model_hash)
+
+    def test_get_algo(self):
+        algo_content, algo_hash = 'content', 'hash'
+        traintuple = {'algo': 'myalgo'}
+        with mock.patch('substrapp.tasks.get_remote_file') as mocked_get_function:
+            mocked_get_function.return_value = algo_content, algo_hash
+            self.assertEqual((algo_content, algo_hash), get_algo(traintuple))
+
+    def test_get_challenge(self):
+        pass
 
     def test_compute_docker(self):
         cpu_set, gpu_set = None, None
@@ -158,3 +266,16 @@ class TasksTests(APITestCase):
         self.assertIn('GPU', result)
         self.assertIn('Mem', result)
         self.assertIn('GPU Mem', result)
+
+    def test_build_traintuple_folders(self):
+        with mock.patch('substrapp.tasks.getattr') as mocked_function:
+            mocked_function.return_value = MEDIA_ROOT
+            key = 'test1234'
+            traintuple_directory = build_traintuple_folders({'key': key})
+            self.assertTrue(os.path.exists(traintuple_directory))
+            self.assertEqual(os.path.join(MEDIA_ROOT, 'traintuple/%s' % key), traintuple_directory)
+
+            for root, dirs, files in os.walk(traintuple_directory):
+                nb_subfolders = len(dirs)
+
+            self.assertTrue(5, nb_subfolders)
