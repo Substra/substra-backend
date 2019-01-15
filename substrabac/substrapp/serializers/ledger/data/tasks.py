9b6813c5346c9bb2a1d9df1e690f513c1c650b89
GuillaumeCisco <guillaumecisco@gmail.com>
2019-01-15 17:54:53 +0100

    Add bulk update data and test

--- substrabac/substrapp/serializers/ledger/data/tasks.py
+++ substrabac/substrapp/serializers/ledger/data/tasks.py
@@ -1,9 +1,13 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from .util import createLedgerData
+from .util import createLedgerData, updateLedgerData
 
 
 @shared_task
 def createLedgerDataAsync(args, pkhashes):
     return createLedgerData(args, pkhashes)
+
+@shared_task
+def updateLedgerDataAsync(args, pkhashes):
+    return updateLedgerData(args, pkhashes)\
