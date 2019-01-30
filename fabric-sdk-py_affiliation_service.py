bb9f232d59191e95cb71e5912a5f0b788adfbd22
GuillaumeCisco <guillaumecisco@gmail.com>
2019-01-30 17:54:56 +0100

    Add identity and affiliations test

---
+++ fabric-sdk-py_affiliation_service.py
@@ -0,0 +1,51 @@
+import random
+import string
+from pprint import pprint
+
+from hfc.fabric_ca.caservice import ca_service
+
+cacli = ca_service(target="https://rca-owkin:7054",
+                   ca_certs_path='/substra/data/orgs/owkin/ca-cert.pem',
+                   ca_name='rca-owkin')
+
+print('Will try to enroll admin')
+try:
+    bootstrap_admin = cacli.enroll('admin', 'adminpw')
+except ValueError as e:
+    print(e)
+else:
+    print('Admin successfully enrolled')
+
+    print('Create affiliation Service')
+
+    affiliationService = cacli.newAffiliationService()
+
+    affiliation = 'department3'
+
+    print(f'Will try to create affiliation {affiliation}')
+    res = affiliationService.create(bootstrap_admin, affiliation)
+    pprint(res)
+
+    print(f'Will try to get affiliation {affiliation}')
+    res = affiliationService.getOne(affiliation, bootstrap_admin)
+    pprint(res)
+
+    print('Will try to get all affiliations')
+    res = affiliationService.getAll(bootstrap_admin)
+    print('number of affiliations: ', len(res['result']['affiliations']))
+
+    print(f'Will try to update affiliation {affiliation} with name=\'department3bis\'')
+    res = affiliationService.update(affiliation, bootstrap_admin, name='department3bis')
+    pprint(res)
+
+    print(f'Will try to get affiliation {affiliation} to see changes')
+    res = affiliationService.getOne(affiliation, bootstrap_admin)
+    pprint(res)
+
+    print(f'Will try to delete affiliation {affiliation}')
+    res = affiliationService.delete('department3bis', bootstrap_admin)
+    pprint(res)
+
+    print(f'Will try to get deleted affiliation {affiliation}')
+    res = affiliationService.getOne(affiliation, bootstrap_admin)
+    pprint(res)
