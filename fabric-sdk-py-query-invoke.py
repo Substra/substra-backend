1344a58a98da10f6554d4705d3541474171fc3aa
GuillaumeCisco <guillaumecisco@gmail.com>
2019-02-08 18:31:18 +0100

    Add invoke example

--- fabric-sdk-py-query-invoke.py
+++ fabric-sdk-py-query-invoke.py
@@ -1,3 +1,5 @@
+import json
+import os
 import random
 import string
 from pprint import pprint
@@ -25,34 +27,52 @@ except ValueError as e:
 else:
 
     response = cli.chaincode_query(
-                   requestor=admin_owkin,
-                   channel_name='mychannel',
-                   peer_names=['peer1-owkin'],
-                   args=[],
-                   cc_name='mycc',
-                   cc_version='1.0',
-                   fcn='queryChallenges'
-                   )
+        requestor=admin_owkin,
+        channel_name='mychannel',
+        peer_names=['peer1-owkin'],
+        args=[],
+        cc_name='mycc',
+        cc_version='1.0',
+        fcn='queryChallenges'
+    )
     print(response)
-    #
-    # response = cli.query_installed_chaincodes(
-    #                requestor=admin_owkin,
-    #                peer_names=['peer1-owkin']
-    #                )
-    # print(response)
-    #
-    # response = cli.query_channels(
-    #                requestor=admin_owkin,
-    #                peer_names=['peer1-owkin']
-    #                )
-    # print(response)
-    #
-    # response = cli.query_info(
-    #                requestor=admin_owkin,
-    #                channel_name='mychannel',
-    #                peer_names=['peer1-owkin']
-    #                )
-    # print(response)
 
+    response = cli.query_installed_chaincodes(
+        requestor=admin_owkin,
+        peer_names=['peer1-owkin']
+    )
+    print(response)
 
+    response = cli.query_channels(
+        requestor=admin_owkin,
+        peer_names=['peer1-owkin']
+    )
+    print(response)
 
+    response = cli.query_info(
+        requestor=admin_owkin,
+        channel_name='mychannel',
+        peer_names=['peer1-owkin']
+    )
+    print(response)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    response = cli.chaincode_invoke(
+        requestor=admin_owkin,
+        channel_name='mychannel',
+        peer_names=['peer1-owkin'],
+        args=['ISIC 2018',
+              'ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994',
+              'http://chunantes.substrabac:8001/media/datasets/ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994/opener.py',
+              'Images',
+              'ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994',
+              'http://chunantes.substrabac:8001/media/datasets/ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994/description.md',
+              '',
+              'all'
+              ],
+        cc_name='mycc',
+        cc_version='1.0',
+        fcn='registerDataset'
+    )
+    print(response)
