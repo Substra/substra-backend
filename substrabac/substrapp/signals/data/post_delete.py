fc44c04e0648e894acf97b37fb7e1a82fa1e25a4
GuillaumeCisco <guillaumecisco@gmail.com>
2019-03-22 18:06:08 +0100

    clean data post_delete signal

--- substrabac/substrapp/signals/data/post_delete.py
+++ substrabac/substrapp/signals/data/post_delete.py
@@ -6,5 +6,5 @@ from django.conf import settings
 
 def data_post_delete(sender, instance, **kwargs):
     # remove created folder
-    directory = path.join(getattr(settings, 'MEDIA_ROOT'), 'data/{0}'.format(instance.pk))
+    directory = path.join(getattr(settings, 'MEDIA_ROOT'), 'data', instance.pk)
     rmtree(directory)
