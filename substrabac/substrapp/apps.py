8fa40a6a640d6575077fadbbee0b2d92c2c778ca
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-03-25 18:30:26 +0100

    Fix rename #1

--- substrabac/substrapp/apps.py
+++ substrabac/substrapp/apps.py
@@ -11,15 +11,15 @@ class SubstrappConfig(AppConfig):
         from .signals.datasample.post_delete import data_sample_post_delete
         from .signals.datamanager.post_delete import datamanager_post_delete
         from .signals.model.post_delete import model_post_delete
-        from .signals.datasample.pre_save import data_pre_save
+        from .signals.datasample.pre_save import data_sample_pre_save
 
         # registering signals with the model's string label
-        from substrapp.models import Algo, Objective, Data, DataManager, Model
+        from substrapp.models import Algo, Objective, DataSample, DataManager, Model
 
         post_delete.connect(algo_post_delete, sender=Algo)
         post_delete.connect(objective_post_delete, sender=Objective)
-        post_delete.connect(data_sample_post_delete, sender=Data)
+        post_delete.connect(data_sample_post_delete, sender=DataSample)
         post_delete.connect(datamanager_post_delete, sender=DataManager)
         post_delete.connect(model_post_delete, sender=Model)
 
-        pre_save.connect(data_pre_save, sender=Data)
+        pre_save.connect(data_sample_pre_save, sender=DataSample)
