2247a74a4fed4de51f2c6357b6196431eb54c770
GuillaumeCisco <guillaumecisco@gmail.com>
2019-04-02 16:08:17 +0200

    Fix is_secure in debug mode

--- substrabac/substrapp/management/utils/localRequest.py
+++ substrabac/substrapp/management/utils/localRequest.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class LocalRequest(object):
 
     def is_secure(self):
-        return getattr(settings, 'DEBUG')
+        return not getattr(settings, 'DEBUG')
 
     def get_host(self):
         return getattr(settings, 'SITE_HOST')
