0f21f439742341eb4cc491abb251f4343a4e3dec
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-04-04 15:34:44 +0200

    Update RM to be stateless

--- substrabac/substrapp/job_utils.py
+++ substrabac/substrapp/job_utils.py
@@ -24,12 +24,16 @@ def get_cpu_sets(cpu_count, concurrency):
 
 
 def get_gpu_sets(gpu_list, concurrency):
-    gpu_count = len(gpu_list)
-    gpu_step = max(1, gpu_count // concurrency)
-    gpu_sets = []
 
-    for igpu_start in range(0, gpu_count, gpu_step):
-        gpu_sets.append(','.join(gpu_list[igpu_start: igpu_start + gpu_step]))
+    if gpu_list:
+        gpu_count = len(gpu_list)
+        gpu_step = max(1, gpu_count // concurrency)
+        gpu_sets = []
+
+        for igpu_start in range(0, gpu_count, gpu_step):
+            gpu_sets.append(','.join(gpu_list[igpu_start: igpu_start + gpu_step]))
+    else:
+        gpu_sets = None
 
     return gpu_sets
 
@@ -66,7 +70,7 @@ def filter_resources_sets(used_resources_sets, resources_sets, expand_resources_
                 real_used_resources_sets.append(reduce_resources_set(resources_set))
                 break
 
-    return list(set(real_used_resources_sets))
+    return list(set(resources_sets).difference(set(real_used_resources_sets)))
 
 
 def filter_cpu_sets(used_cpu_sets, cpu_sets):
@@ -77,7 +81,7 @@ def filter_gpu_sets(used_gpu_sets, gpu_sets):
     return filter_resources_sets(used_gpu_sets, gpu_sets, expand_gpu_set, reduce_gpu_set)
 
 
-def update_statistics(job_statistics, stats, gpu_stats):
+def update_statistics(task_statistics, stats, gpu_stats):
 
     # CPU
 
@@ -89,9 +93,9 @@ def update_statistics(job_statistics, stats, gpu_stats):
             delta_system_usage = (stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage'])
             total_usage = (delta_total_usage / delta_system_usage) * stats['cpu_stats']['online_cpus'] * 100.0
 
-            job_statistics['cpu']['current'].append(total_usage)
-            job_statistics['cpu']['max'] = max(job_statistics['cpu']['max'],
-                                               max(job_statistics['cpu']['current']))
+            task_statistics['cpu']['current'].append(total_usage)
+            task_statistics['cpu']['max'] = max(task_statistics['cpu']['max'],
+                                                max(task_statistics['cpu']['current']))
 
         # MEMORY in GB
         if 'memory_stats' in stats:
@@ -99,83 +103,62 @@ def update_statistics(job_statistics, stats, gpu_stats):
             max_usage = stats['memory_stats'].get('max_usage', None)
 
             if current_usage:
-                job_statistics['memory']['current'].append(current_usage / 1024**3)
+                task_statistics['memory']['current'].append(current_usage / 1024**3)
             if max_usage:
-                job_statistics['memory']['max'] = max(job_statistics['memory']['max'],
-                                                      max_usage / 1024**3,
-                                                      max(job_statistics['memory']['current']))
+                task_statistics['memory']['max'] = max(task_statistics['memory']['max'],
+                                                       max_usage / 1024**3,
+                                                       max(task_statistics['memory']['current']))
 
         # Network in kB
         if 'networks' in stats:
-            job_statistics['netio']['rx'] = stats['networks']['eth0'].get('rx_bytes', 0)
-            job_statistics['netio']['tx'] = stats['networks']['eth0'].get('tx_bytes', 0)
+            task_statistics['netio']['rx'] = stats['networks']['eth0'].get('rx_bytes', 0)
+            task_statistics['netio']['tx'] = stats['networks']['eth0'].get('tx_bytes', 0)
 
     # GPU
 
     if gpu_stats is not None:
         total_usage = sum([100 * gpu.load for gpu in gpu_stats])
-        job_statistics['gpu']['current'].append(total_usage)
-        job_statistics['gpu']['max'] = max(job_statistics['gpu']['max'],
-                                           max(job_statistics['gpu']['current']))
+        task_statistics['gpu']['current'].append(total_usage)
+        task_statistics['gpu']['max'] = max(task_statistics['gpu']['max'],
+                                            max(task_statistics['gpu']['current']))
 
         total_usage = sum([gpu.memoryUsed for gpu in gpu_stats]) / 1024
-        job_statistics['gpu_memory']['current'].append(total_usage)
-        job_statistics['gpu_memory']['max'] = max(job_statistics['gpu_memory']['max'],
-                                                  max(job_statistics['gpu_memory']['current']))
-
-    # IO DISK
-    # "blkio_stats": {
-    #   "io_service_bytes_recursive": [],
-    #   "io_serviced_recursive": [],
-    #   "io_queue_recursive": [],
-    #   "io_service_time_recursive": [],
-    #   "io_wait_time_recursive": [],
-    #   "io_merged_recursive": [],
-    #   "io_time_recursive": [],
-    #   "sectors_recursive": []
-    # }
-
-    # LOGGING
-    # printable_stats = 'CPU - now : %d %% / max : %d %% | MEM - now : %.2f GB / max : %.2f GB' % \
-    #     (job_statistics['cpu']['current'][-1],
-    #      job_statistics['cpu']['max'],
-    #      job_statistics['memory']['current'][-1],
-    #      job_statistics['memory']['max'])
-
-    # logging.info('[JOB] Monitoring : %s' % (printable_stats, ))
+        task_statistics['gpu_memory']['current'].append(total_usage)
+        task_statistics['gpu_memory']['max'] = max(task_statistics['gpu_memory']['max'],
+                                                   max(task_statistics['gpu_memory']['current']))
 
 
-def monitoring_job(client, job_args):
+def monitoring_task(client, task_args):
     """thread worker function"""
 
-    job_name = job_args['name']
+    task_name = task_args['name']
 
     gpu_set = None
-    if 'environment' in job_args:
-        gpu_set = job_args['environment']['NVIDIA_VISIBLE_DEVICES']
+    if 'environment' in task_args:
+        gpu_set = task_args['environment']['NVIDIA_VISIBLE_DEVICES']
 
     start = time.time()
     t = threading.currentThread()
 
     # Statistics
-    job_statistics = {'memory': {'max': 0,
-                                 'current': [0]},
-                      'gpu_memory': {'max': 0,
-                                     'current': [0]},
-                      'cpu': {'max': 0,
-                              'current': [0]},
-                      'gpu': {'max': 0,
+    task_statistics = {'memory': {'max': 0,
+                                  'current': [0]},
+                       'gpu_memory': {'max': 0,
+                                      'current': [0]},
+                       'cpu': {'max': 0,
+                               'current': [0]},
+                       'gpu': {'max': 0,
+                               'current': []},
+                       'io': {'max': 0,
                               'current': []},
-                      'io': {'max': 0,
-                             'current': []},
-                      'netio': {'rx': 0,
-                                'tx': 0},
-                      'time': 0}
+                       'netio': {'rx': 0,
+                                 'tx': 0},
+                       'time': 0}
 
-    while getattr(t, "do_run", True):
+    while getattr(t, "alive", True):
         stats = None
         try:
-            container = client.containers.get(job_name)
+            container = client.containers.get(task_name)
             stats = container.stats(decode=True, stream=False)
         except:
             pass
@@ -184,17 +167,17 @@ def monitoring_job(client, job_args):
         if gpu_set is not None:
             gpu_stats = [gpu for gpu in gputil.getGPUs() if str(gpu.id) in gpu_set]
 
-        update_statistics(job_statistics, stats, gpu_stats)
+        update_statistics(task_statistics, stats, gpu_stats)
 
-    job_statistics['time'] = time.time() - start
+    task_statistics['time'] = time.time() - start
 
-    t._result = f"CPU:{job_statistics['cpu']['max']:.2f} % - Mem:{job_statistics['memory']['max']:.2f} GB - GPU:{job_statistics['gpu']['max']:.2f} % - GPU Mem:{job_statistics['gpu_memory']['max']:.2f} GB"
+    t._result = f"CPU:{task_statistics['cpu']['max']:.2f} % - Mem:{task_statistics['memory']['max']:.2f}"
+    t._result += f" GB - GPU:{task_statistics['gpu']['max']:.2f} % - GPU Mem:{task_statistics['gpu_memory']['max']:.2f} GB"
 
-    t._stats = job_statistics
+    t._stats = task_statistics
 
 
-def compute_docker(client, resources_manager, dockerfile_path, image_name, container_name, volumes, command, cpu_set, gpu_set,
-                   remove_image=True):
+def compute_docker(client, resources_manager, dockerfile_path, image_name, container_name, volumes, command, remove_image=True):
 
     dockerfile_fullpath = os.path.join(dockerfile_path, 'Dockerfile')
     if not os.path.exists(dockerfile_fullpath):
@@ -204,51 +187,39 @@ def compute_docker(client, resources_manager, dockerfile_path, image_name, conta
     client.images.build(path=dockerfile_path,
                         tag=image_name,
                         rm=remove_image)
-    cpu_set = None
-    gpu_set = None
 
-    mem_limit = resources_manager.memory_limit_mb()
+    # Limit ressources
+    memory_limit_mb = f'{resources_manager.memory_limit_mb()}M'
+    cpu_set, gpu_set = resources_manager.get_cpu_gpu_sets()    # blocking call
 
-    while cpu_set is None or gpu_set is None:
-        resources_manager.sync()
-        cpu_set = resources_manager.acquire_cpu_set()
-        gpu_set = resources_manager.acquire_gpu_set()
-        time.sleep(2)
+    task_args = {'image': image_name,
+                 'name': container_name,
+                 'cpuset_cpus': cpu_set,
+                 'mem_limit': memory_limit_mb,
+                 'command': command,
+                 'volumes': volumes,
+                 'shm_size': '8G',
+                 'labels': [DOCKER_LABEL],
+                 'detach': False,
+                 'auto_remove': False,
+                 'remove': False}
 
-    job_args = {'image': image_name,
-                'name': container_name,
-                'cpuset_cpus': cpu_set,
-                'mem_limit': mem_limit,
-                'command': command,
-                'volumes': volumes,
-                #'ipc_mode': 'host',    # Need to check if it safe to do that, we need it for torch
-                'shm_size': '8G',
-                'labels': [DOCKER_LABEL],
-                'detach': False,
-                'auto_remove': False,
-                'remove': False}
+    if gpu_set is not None:
+        task_args['environment'] = {'NVIDIA_VISIBLE_DEVICES': gpu_set}
+        task_args['runtime'] = 'nvidia'
 
-    if gpu_set != 'no_gpu':
-        job_args['environment'] = {'NVIDIA_VISIBLE_DEVICES': gpu_set}
-        job_args['runtime'] = 'nvidia'
+    task = ExceptionThread(target=client.containers.run,
+                           kwargs=task_args)
 
-    job = ExceptionThread(target=client.containers.run,
-                          kwargs=job_args)
-    monitoring = ExceptionThread(target=monitoring_job, args=(client, job_args))
+    monitoring = ExceptionThread(target=monitoring_task,
+                                 args=(client, task_args))
 
-    job.start()
+    task.start()
     monitoring.start()
 
-    job.join()
-    monitoring.do_run = False
+    task.join()
+    monitoring.alive = False
     monitoring.join()
-
-    # Return resources
-    resources_manager.return_cpu_set(cpu_set)
-    resources_manager.return_gpu_set(gpu_set)
-
-    cpu_set = None
-    gpu_set = None
 
     # Remove container in all case (exception thrown or not)
     # We already get the excetion and we need to remove the containers to be able to remove the local
@@ -257,11 +228,11 @@ def compute_docker(client, resources_manager, dockerfile_path, image_name, conta
     container.remove()
 
     # Remove images
-    if remove_image or hasattr(job, "_exception"):
+    if remove_image or hasattr(task, "_exception"):
         client.images.remove(image_name, force=True)
 
-    if hasattr(job, "_exception"):
-        raise job._exception
+    if hasattr(task, "_exception"):
+        raise task._exception
 
     return monitoring._result
 
@@ -284,7 +255,8 @@ class ExceptionThread(threading.Thread):
 class ResourcesManager():
 
     __concurrency = int(getattr(settings, 'CELERY_WORKER_CONCURRENCY'))
-    __memory_gb = int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024. ** 2))
+    __memory_mb = int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024. ** 2))
+    __memory_mb_per_task = __memory_mb // __concurrency
 
     __cpu_count = os.cpu_count()
     __cpu_sets = get_cpu_sets(__cpu_count, __concurrency)
@@ -292,105 +264,60 @@ class ResourcesManager():
     # Set CUDA_DEVICE_ORDER so the IDs assigned by CUDA match those from nvidia-smi
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     __gpu_list = [str(gpu.id) for gpu in gputil.getGPUs()]
+    __gpu_sets = get_gpu_sets(__gpu_list, __concurrency)  # Can be None if no gpu
 
-    __gpu_sets = 'no_gpu'
-    if __gpu_list:
-        __gpu_sets = get_gpu_sets(__gpu_list, __concurrency)
-
-    __used_cpu_sets = []
-    __used_gpu_sets = []
     __lock = threading.Lock()
     __docker = docker.from_env()
 
     @classmethod
     def memory_limit_mb(cls):
-        return f'{cls.__memory_gb // cls.__concurrency}M'
+        return cls.__memory_mb_per_task
 
     @classmethod
-    def acquire_cpu_set(cls):
+    def get_cpu_gpu_sets(cls):
+
         cpu_set = None
-        cls.__lock.acquire()
+        gpu_set = None
 
-        try:
-            cpu_set_available = set(cls.__cpu_sets).difference(set(cls.__used_cpu_sets))
-            if cpu_set_available:
-                cpu_set = cpu_set_available.pop()
-                cls.__used_cpu_sets.append(cpu_set)
-        except Exception as e:
-            logging.error(e, exc_info=True)
+        with cls.__lock:
+            # We can just wait for cpu because cpu and gpu is allocated the same way
+            while cpu_set is None:
+                try:
 
-        cls.__lock.release()
-        return cpu_set
+                    # Get ressources used
+                    filters = {'status': 'running', 'label': [DOCKER_LABEL]}
+                    containers = [container.attrs
+                                  for container in cls.__docker.containers.list(filters=filters)]
 
-    @classmethod
-    def return_cpu_set(cls, cpu_set):
-        cls.__lock.acquire()
+                    # CPU
+                    used_cpu_sets = [container['HostConfig']['CpusetCpus']
+                                     for container in containers
+                                     if container['HostConfig']['CpusetCpus']]
 
-        try:
-            if cpu_set in cls.__used_cpu_sets:
-                cls.__used_cpu_sets.remove(cpu_set)
-        except Exception as e:
-            logging.error(e, exc_info=True)
+                    cpu_sets_available = filter_cpu_sets(used_cpu_sets, cls.__cpu_sets)
 
-        cls.__lock.release()
+                    if cpu_sets_available:
+                        cpu_set = cpu_sets_available.pop()
 
-    @classmethod
-    def acquire_gpu_set(cls):
-        gpu_set = 'no_gpu'
-        cls.__lock.acquire()
+                    # GPU
+                    if cls.__gpu_sets is not None:
+                        env_containers = [container['Config']['Env']
+                                          for container in containers]
 
-        if cls.__gpu_sets != 'no_gpu':
-            gpu_set = None
-            try:
-                gpu_set_available = set(cls.__gpu_sets).difference(set(cls.__used_gpu_sets))
-                if gpu_set_available:
-                    gpu_set = gpu_set_available.pop()
-                    cls.__used_gpu_sets.append(gpu_set)
-            except Exception as e:
-                logging.error(e, exc_info=True)
+                        used_gpu_sets = []
 
-        cls.__lock.release()
-        return gpu_set
+                        for env_list in env_containers:
+                            nvidia_env_var = [s.split('=')[1]
+                                              for s in env_list if "NVIDIA_VISIBLE_DEVICES" in s]
 
-    @classmethod
-    def return_gpu_set(cls, gpu_set):
-        cls.__lock.acquire()
+                            used_gpu_sets.extend(nvidia_env_var)
 
-        if gpu_set != 'no_gpu':
-            try:
-                if gpu_set in cls.__used_gpu_sets:
-                    cls.__used_gpu_sets.remove(gpu_set)
-            except Exception as e:
-                logging.error(e, exc_info=True)
+                        gpu_sets_available = filter_gpu_sets(used_gpu_sets, cls.__gpu_sets)
 
-        cls.__lock.release()
+                        if gpu_sets_available:
+                            gpu_set = gpu_sets_available.pop()
 
-    @classmethod
-    def sync(cls):
-        """ Synchronise resources manager with running job
-        """
-        cls.__lock.acquire()
+                except Exception as e:
+                    logging.error(e, exc_info=True)
 
-        try:
-            filters = {'status': 'running', 'label': [DOCKER_LABEL]}
-            containers = [container.attrs for container in cls.__docker.containers.list(filters=filters)]
-
-            # cpu
-            used_cpu_sets = [container['HostConfig']['CpusetCpus'] for container in containers if container['HostConfig']['CpusetCpus']]
-            cls.__used_cpu_sets = filter_cpu_sets(used_cpu_sets, cls.__cpu_sets)
-
-            # gpu
-            if cls.__gpu_sets != 'no_gpu':
-                env_containers = [container['Config']['Env'] for container in containers]
-
-                used_gpu_sets = []
-                for env_list in env_containers:
-                    nvidia_env_var = [s.split('=')[1] for s in env_list if "NVIDIA_VISIBLE_DEVICES" in s]
-                    used_gpu_sets.extend(nvidia_env_var)
-
-                cls.__used_gpu_sets = filter_gpu_sets(used_gpu_sets, cls.__gpu_sets)
-
-        except Exception as e:
-            logging.error(e, exc_info=True)
-
-        cls.__lock.release()
+        return cpu_set, gpu_set
