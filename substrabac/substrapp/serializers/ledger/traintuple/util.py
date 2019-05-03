6be55010b340464de2ac7e8a0863e684c9e69cb5
GuillaumeCisco <guillaumecisco@gmail.com>
2019-05-03 15:58:23 +0200

    save progress

--- substrabac/substrapp/serializers/ledger/traintuple/util.py
+++ substrabac/substrapp/serializers/ledger/traintuple/util.py
@@ -5,4 +5,4 @@ from substrapp.utils import invokeLedger
 
 
 def createLedgerTraintuple(args, sync=False):
-    return invokeLedger(fcn='createTraintuple', args=args, sync=sync)
+    return invokeLedger(fcn='createTraintuple', args=args, cc_pattern='traintuple-creation', sync=sync)
