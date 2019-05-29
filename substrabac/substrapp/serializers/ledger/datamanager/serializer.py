5ce468380b27f43c587ac447bd6d1969c27a6a10
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-05-29 11:06:12 +0200

    Update and clean ledger serializer.

--- substrabac/substrapp/serializers/ledger/datamanager/serializer.py
+++ substrabac/substrapp/serializers/ledger/datamanager/serializer.py
@@ -10,28 +10,32 @@ from .tasks import createLedgerDataManagerAsync
 
 class LedgerDataManagerSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
-    type = serializers.CharField(max_length=30)
+    data_type = serializers.CharField(max_length=30)
     objective_key = serializers.CharField(max_length=256, allow_blank=True, required=False)
     permissions = serializers.CharField(min_length=1, max_length=60)
 
     def create(self, validated_data):
         instance = self.initial_data.get('instance')
         name = validated_data.get('name')
-        type = validated_data.get('type')
+        data_type = validated_data.get('type')
         permissions = validated_data.get('permissions')
         objective_key = validated_data.get('objective_key', '')
 
         # TODO, create a datamigration with new Site domain name when we will know the name of the final website
-        # current_site = Site.objects.get_current()
+        host = ''
+        protocol = 'http://'
         request = self.context.get('request', None)
-        protocol = 'https://' if request.is_secure() else 'http://'
-        host = '' if request is None else request.get_host()
 
-        # args = '"%(name)s", "%(openerHash)s", "%(openerStorageAddress)s", "%(type)s", "%(descriptionHash)s", "%(descriptionStorageAddress)s", "%(objectiveKey)s", "%(permissions)s"' % {  # noqa
+        if request:
+            protocol = 'https://' if request.is_secure() else 'http://'
+            host = request.get_host()
+
+        # Json
+        # args = {
         #     'name': name,
         #     'openerHash': get_hash(instance.data_opener),
         #     'openerStorageAddress': protocol + host + reverse('substrapp:data_manager-opener', args=[instance.pk]),
-        #     'type': type,
+        #     'type': data_type,
         #     'descriptionHash': get_hash(instance.description),
         #     'descriptionStorageAddress': protocol + host + reverse('substrapp:data_manager-description', args=[instance.pk]),  # noqa
         #     'objectiveKey': objective_key,
@@ -42,7 +46,7 @@ class LedgerDataManagerSerializer(serializers.Serializer):
             name,
             get_hash(instance.data_opener),
             protocol + host + reverse('substrapp:data_manager-opener', args=[instance.pk]),
-            type,
+            data_type,
             get_hash(instance.description),
             protocol + host + reverse('substrapp:data_manager-description', args=[instance.pk]),
             objective_key,
@@ -59,5 +63,4 @@ class LedgerDataManagerSerializer(serializers.Serializer):
                 'message': 'DataManager added in local db waiting for validation. '
                            'The substra network has been notified for adding this DataManager'
             }
-            st = status.HTTP_202_ACCEPTED
-            return data, st
+            return data, status.HTTP_202_ACCEPTED
