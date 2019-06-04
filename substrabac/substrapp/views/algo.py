be2d5c9d398fb6c4ca3347446af14a8e1fc99607
Samuel <samuel.lesuffleur@owkin.com>
2019-06-04 10:26:54 +0200

    refactor algo views

--- substrabac/substrapp/views/algo.py
+++ substrabac/substrapp/views/algo.py
@@ -13,7 +13,8 @@ from substrapp.models import Algo
 from substrapp.serializers import LedgerAlgoSerializer, AlgoSerializer
 from substrapp.utils import get_hash, JsonException
 from substrapp.ledger_utils import queryLedger, getObjectFromLedger
-from substrapp.views.utils import ComputeHashMixin, ManageFileMixin, find_primary_key_error
+from substrapp.views.utils import (ComputeHashMixin, ManageFileMixin, find_primary_key_error,
+                                   validate_pk)
 from substrapp.views.filters_utils import filter_list
 
 
@@ -31,14 +32,13 @@ class AlgoViewSet(mixins.CreateModelMixin,
         return serializer.save()
 
     def create(self, request, *args, **kwargs):
-        data = request.data
 
-        file = data.get('file')
+        file = request.data.get('file')
         pkhash = get_hash(file)
         serializer = self.get_serializer(data={
             'pkhash': pkhash,
             'file': file,
-            'description': data.get('description')
+            'description': request.data.get('description')
         })
 
         try:
@@ -48,120 +48,104 @@ class AlgoViewSet(mixins.CreateModelMixin,
             if find_primary_key_error(e):
                 st = status.HTTP_409_CONFLICT
             return Response({'message': e.args, 'pkhash': pkhash}, status=st)
-        else:
 
-            # create on db
-            try:
-                instance = self.perform_create(serializer)
-            except Exception as exc:
-                return Response({'message': exc.args},
-                                status=status.HTTP_400_BAD_REQUEST)
-            else:
-                # init ledger serializer
-                ledger_serializer = LedgerAlgoSerializer(data={'name': data.get('name'),
-                                                               'permissions': data.get('permissions', 'all'),
-                                                               'instance': instance},
-                                                         context={'request': request})
-                if not ledger_serializer.is_valid():
-                    # delete instance
-                    instance.delete()
-                    raise ValidationError(ledger_serializer.errors)
+        # create on db
+        try:
+            instance = self.perform_create(serializer)
+        except Exception as e:
+            return Response({'message': e.args},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-                # create on ledger
-                data, st = ledger_serializer.create(ledger_serializer.validated_data)
+        # init ledger serializer
+        ledger_serializer = LedgerAlgoSerializer(data={
+            'name': request.data.get('name'),
+            'permissions': request.data.get('permissions', 'all'),
+            'instance': instance
+        }, context={'request': request})
+        if not ledger_serializer.is_valid():
+            # delete instance
+            instance.delete()
+            raise ValidationError(ledger_serializer.errors)
 
-                if st not in (status.HTTP_201_CREATED, status.HTTP_202_ACCEPTED, status.HTTP_408_REQUEST_TIMEOUT):
-                    return Response(data, status=st)
+        # create on ledger
+        data, st = ledger_serializer.create(ledger_serializer.validated_data)
 
-                headers = self.get_success_headers(serializer.data)
-                d = dict(serializer.data)
-                d.update(data)
-                return Response(d, status=st, headers=headers)
+        if st not in (status.HTTP_201_CREATED, status.HTTP_202_ACCEPTED, status.HTTP_408_REQUEST_TIMEOUT):
+            return Response(data, status=st)
+
+        headers = self.get_success_headers(serializer.data)
+        d = dict(serializer.data)
+        d.update(data)
+        return Response(d, status=st, headers=headers)
 
     def create_or_update_algo(self, algo, pk):
+        # get algo description from remote node
+        url = algo['description']['storageAddress']
         try:
-            # get algo description from remote node
-            url = algo['description']['storageAddress']
-            try:
-                r = requests.get(url, headers={'Accept': 'application/json;version=0.0'})  # TODO pass cert
-            except Exception:
-                raise Exception(f'Failed to fetch {url}')
-            else:
-                if r.status_code != 200:
-                    raise Exception(f'end to end node report {r.text}')
-
-                try:
-                    computed_hash = self.compute_hash(r.content)
-                except Exception:
-                    raise Exception('Failed to fetch description file')
-                else:
-                    if computed_hash != algo['description']['hash']:
-                        msg = 'computed hash is not the same as the hosted file. ' \
-                              'Please investigate for default of synchronization, corruption, or hacked'
-                        raise Exception(msg)
-
-                    f = tempfile.TemporaryFile()
-                    f.write(r.content)
-
-                    # save/update objective in local db for later use
-                    instance, created = Algo.objects.update_or_create(pkhash=pk, validated=True)
-                    instance.description.save('description.md', f)
+            r = requests.get(url, headers={'Accept': 'application/json;version=0.0'})  # TODO pass cert
         except Exception as e:
-            raise e
-        else:
-            return instance
+            raise Exception(f'Failed to fetch {url}') from e
+
+        if r.status_code != 200:
+            logging.error(f'Request failed: {r.text}')
+            raise Exception(f'Failed to fetch {url}: invalid status code {r.status_code}')
+
+        try:
+            computed_hash = self.compute_hash(r.content)
+        except Exception as e:
+            raise Exception('Failed to fetch description file') from e
+
+        if computed_hash != algo['description']['hash']:
+            msg = 'computed hash is not the same as the hosted file. ' \
+                  'Please investigate for default of synchronization, corruption, or hacked'
+            raise Exception(msg)
+
+        f = tempfile.TemporaryFile()
+        f.write(r.content)
+
+        # save/update objective in local db for later use
+        instance, created = Algo.objects.update_or_create(pkhash=pk, validated=True)
+        instance.description.save('description.md', f)
+
+        return instance
 
     def retrieve(self, request, *args, **kwargs):
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
         pk = self.kwargs[lookup_url_kwarg]
 
-        if len(pk) != 64:
-            return Response({'message': f'Wrong pk {pk}'}, status.HTTP_400_BAD_REQUEST)
-
         try:
-            int(pk, 16)  # test if pk is correct (hexadecimal)
-        except Exception:
-            return Response({'message': f'Wrong pk {pk}'}, status.HTTP_400_BAD_REQUEST)
-        else:
-            # get instance from remote node
-            error = None
+            validate_pk(pk)
+        except Exception as e:
+            return Response({'message': str(e)}, status.HTTP_400_BAD_REQUEST)
+
+        # get instance from remote node
+        try:
+            data = getObjectFromLedger(pk, self.ledger_query_call)
+        except JsonException as e:
+            return Response(e.msg, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+            return Response(f'No element with key {pk}', status=status.HTTP_404_NOT_FOUND)
+
+        # try to get it from local db to check if description exists
+        try:
+            instance = self.get_object()
+        except Http404:
             instance = None
+
+        # check if instance has description
+        if not instance or not instance.description:
             try:
-                data = getObjectFromLedger(pk, self.ledger_query_call)
-            except JsonException as e:
-                return Response(e.msg, status=status.HTTP_400_BAD_REQUEST)
-            except Http404:
-                return Response(f'No element with key {pk}', status=status.HTTP_404_NOT_FOUND)
-            else:
-                try:
-                    # try to get it from local db to check if description exists
-                    instance = self.get_object()
-                except Http404:
-                    try:
-                        instance = self.create_or_update_algo(data, pk)
-                    except Exception as e:
-                        error = e
-                else:
-                    # check if instance has description
-                    if not instance.description:
-                        try:
-                            instance = self.create_or_update_algo(data, pk)
-                        except Exception as e:
-                            error = e
-                finally:
-                    if error is not None:
-                        return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
+                instance = self.create_or_update_algo(data, pk)
+            except Exception as e:
+                logging.exception(e)
+                return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
-                    # do not give access to local files address
-                    if instance is not None:
-                        serializer = self.get_serializer(
-                            instance,
-                            fields=('owner', 'pkhash', 'creation_date', 'last_modified'))
-                        data.update(serializer.data)
-                    else:
-                        data = {'message': 'Fail to get instance'}
-
-                    return Response(data, status=status.HTTP_200_OK)
+        # do not give access to local files address
+        serializer = self.get_serializer(
+            instance,
+            fields=('owner', 'pkhash', 'creation_date', 'last_modified'))
+        data.update(serializer.data)
+        return Response(data, status=status.HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
         data, st = queryLedger(fcn='queryAlgos', args=[])
