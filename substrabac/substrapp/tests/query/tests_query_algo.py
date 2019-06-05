34d8b626e1af02a57c7ba0d6cbeef9f2d02bfe47
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-06-05 12:12:42 +0200

    Update query tests.

---
+++ substrabac/substrapp/tests/query/tests_query_algo.py
@@ -0,0 +1,227 @@
+import os
+import shutil
+import tempfile
+
+import mock
+
+from django.urls import reverse
+from django.test import override_settings
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from substrapp.models import Objective, Algo
+from substrapp.serializers import LedgerAlgoSerializer
+from substrapp.utils import get_hash, compute_hash
+
+from ..common import get_sample_objective, get_sample_datamanager, \
+    get_sample_algo
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+@override_settings(LEDGER_SYNC_ENABLED=True)
+class AlgoQueryTests(APITestCase):
+
+    def setUp(self):
+        if not os.path.exists(MEDIA_ROOT):
+            os.makedirs(MEDIA_ROOT)
+
+        self.objective_description, self.objective_description_filename, \
+            self.objective_metrics, self.objective_metrics_filename = get_sample_objective()
+
+        self.algo, self.algo_filename = get_sample_algo()
+
+        self.data_description, self.data_description_filename, self.data_data_opener, \
+            self.data_opener_filename = get_sample_datamanager()
+
+    def tearDown(self):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+
+    def add_default_objective(self):
+        Objective.objects.create(description=self.objective_description,
+                                 metrics=self.objective_metrics)
+
+    def get_default_algo_data(self):
+        expected_hash = get_hash(self.algo)
+
+        data = {
+            'file': self.algo,
+            'description': self.data_description,  # fake it
+            'name': 'super top algo',
+            'objective_key': get_hash(self.objective_description),
+            'permissions': 'all'
+        }
+
+        return expected_hash, data
+
+    def test_add_algo_sync_ok(self):
+        self.add_default_objective()
+        pkhash, data = self.get_default_algo_data()
+
+        url = reverse('substrapp:algo-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+
+        with mock.patch('substrapp.serializers.ledger.algo.util.invoke_ledger') as minvoke_ledger:
+            minvoke_ledger.return_value = {'pkhash': pkhash}, status.HTTP_201_CREATED
+
+            response = self.client.post(url, data, format='multipart', **extra)
+            r = response.json()
+
+            self.assertEqual(r['pkhash'], pkhash)
+            self.assertEqual(r['validated'], True)
+            self.assertEqual(r['description'],
+                             f'http://testserver/media/algos/{r["pkhash"]}/{self.data_description_filename}')
+            self.assertEqual(r['file'],
+                             f'http://testserver/media/algos/{r["pkhash"]}/{self.algo_filename}')
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @override_settings(LEDGER_SYNC_ENABLED=False)
+    @override_settings(
+        task_eager_propagates=True,
+        task_always_eager=True,
+        broker_url='memory://',
+        backend='memory'
+    )
+    def test_add_algo_no_sync_ok(self):
+        self.add_default_objective()
+        pkhash, data = self.get_default_algo_data()
+
+        url = reverse('substrapp:algo-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+        with mock.patch('substrapp.serializers.ledger.algo.util.invoke_ledger') as minvoke_ledger:
+            minvoke_ledger.return_value = ({
+                'message': 'Algo added in local db waiting for validation.'
+                           'The substra network has been notified for adding this Algo'
+            }, status.HTTP_202_ACCEPTED)
+            response = self.client.post(url, data, format='multipart', **extra)
+            r = response.json()
+
+            self.assertEqual(r['pkhash'], pkhash)
+            self.assertEqual(r['validated'], False)
+            self.assertEqual(r['description'],
+                             f'http://testserver/media/algos/{r["pkhash"]}/{self.data_description_filename}')
+            self.assertEqual(r['file'],
+                             f'http://testserver/media/algos/{r["pkhash"]}/{self.algo_filename}')
+            self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_add_algo_ko(self):
+        url = reverse('substrapp:algo-list')
+
+        # non existing associated objective
+        data = {
+            'file': self.algo,
+            'description': self.data_description,
+            'name': 'super top algo',
+            'objective_key': 'non existing objectivexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            'permissions': 'all'
+        }
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+
+        with mock.patch.object(LedgerAlgoSerializer, 'create') as mcreate:
+            mcreate.return_value = {
+                'message': 'Fail to add algo. Objective does not exist'}, status.HTTP_400_BAD_REQUEST
+
+            response = self.client.post(url, data, format='multipart', **extra)
+            r = response.json()
+            self.assertIn('does not exist', r['message'])
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+            Objective.objects.create(description=self.objective_description,
+                                     metrics=self.objective_metrics)
+
+            # missing local storage field
+            data = {
+                'name': 'super top algo',
+                'objective_key': get_hash(self.objective_description),
+                'permissions': 'all'
+            }
+            response = self.client.post(url, data, format='multipart', **extra)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+            # missing ledger field
+            data = {
+                'file': self.algo,
+                'description': self.data_description,
+                'objective_key': get_hash(self.objective_description),
+            }
+            response = self.client.post(url, data, format='multipart', **extra)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_algo_no_version(self):
+
+        self.add_default_objective()
+
+        url = reverse('substrapp:algo-list')
+
+        data = {
+            'file': self.algo,
+            'description': self.data_description,
+            'name': 'super top algo',
+            'objective_key': get_hash(self.objective_description),
+            'permissions': 'all'
+        }
+        response = self.client.post(url, data, format='multipart')
+        r = response.json()
+
+        self.assertEqual(r, {'detail': 'A version is required.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_add_algo_wrong_version(self):
+
+        self.add_default_objective()
+
+        url = reverse('substrapp:algo-list')
+
+        data = {
+            'file': self.algo,
+            'description': self.data_description,
+            'name': 'super top algo',
+            'objective_key': get_hash(self.objective_description),
+            'permissions': 'all'
+        }
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=-1.0',
+        }
+        response = self.client.post(url, data, format='multipart', **extra)
+        r = response.json()
+
+        self.assertEqual(r, {'detail': 'Invalid version in "Accept" header.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_get_algo_files(self):
+        algo = Algo.objects.create(file=self.algo)
+        with mock.patch(
+                'substrapp.views.utils.get_object_from_ledger') as mget_object_from_ledger:
+            mget_object_from_ledger.return_value = self.algo
+            extra = {
+                'HTTP_ACCEPT': 'application/json;version=0.0',
+            }
+            response = self.client.get(f'/algo/{algo.pkhash}/file/', **extra)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(algo.pkhash, compute_hash(response.getvalue()))
+            # self.assertEqual(r, f'http://testserver/media/algos/{algo.pkhash}/{self.algo_filename}')
+
+    def test_get_algo_files_no_version(self):
+        algo = Algo.objects.create(file=self.algo)
+        response = self.client.get(f'/algo/{algo.pkhash}/file/')
+        r = response.json()
+        self.assertEqual(r, {'detail': 'A version is required.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_get_algo_files_wrong_version(self):
+        algo = Algo.objects.create(file=self.algo)
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=-1.0',
+        }
+        response = self.client.get(f'/algo/{algo.pkhash}/file/', **extra)
+        r = response.json()
+        self.assertEqual(r, {'detail': 'Invalid version in "Accept" header.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
---
+++ substrabac/substrapp/tests/query/tests_query_datamanager.py
@@ -0,0 +1,133 @@
+import os
+import shutil
+import tempfile
+
+import mock
+
+from django.urls import reverse
+from django.test import override_settings
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+from substrapp.utils import get_hash
+
+from ..common import get_sample_datamanager
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+@override_settings(LEDGER_SYNC_ENABLED=True)
+class DataManagerQueryTests(APITestCase):
+
+    def setUp(self):
+        if not os.path.exists(MEDIA_ROOT):
+            os.makedirs(MEDIA_ROOT)
+
+        self.data_description, self.data_description_filename, self.data_data_opener, \
+            self.data_opener_filename = get_sample_datamanager()
+
+    def tearDown(self):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+
+    def get_default_datamanager_data(self):
+        expected_hash = get_hash(self.data_data_opener)
+        data = {
+            'name': 'slide opener',
+            'type': 'images',
+            'permissions': 'all',
+            'objective_key': '',
+            'description': self.data_description,
+            'data_opener': self.data_data_opener
+        }
+        return expected_hash, data
+
+    def test_add_datamanager_sync_ok(self):
+
+        pkhash, data = self.get_default_datamanager_data()
+
+        url = reverse('substrapp:data_manager-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+
+        with mock.patch('substrapp.serializers.ledger.datamanager.util.invoke_ledger') as minvoke_ledger:
+            minvoke_ledger.return_value = ({'pkhash': pkhash}, status.HTTP_201_CREATED)
+
+            response = self.client.post(url, data, format='multipart', **extra)
+            r = response.json()
+
+            self.assertEqual(r['pkhash'], pkhash)
+            self.assertEqual(r['validated'], True)
+            self.assertEqual(r['description'],
+                             f'http://testserver/media/datamanagers/{r["pkhash"]}/{self.data_description_filename}')
+
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @override_settings(LEDGER_SYNC_ENABLED=False)
+    @override_settings(
+        task_eager_propagates=True,
+        task_always_eager=True,
+        broker_url='memory://',
+        backend='memory'
+    )
+    def test_add_datamanager_no_sync_ok(self):
+
+        pkhash, data = self.get_default_datamanager_data()
+
+        url = reverse('substrapp:data_manager-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+
+        with mock.patch('substrapp.serializers.ledger.datamanager.util.invoke_ledger') as minvoke_ledger:
+            minvoke_ledger.return_value = ({
+                'message': 'DataManager added in local db waiting for validation.'
+                           'The substra network has been notified for adding this DataManager'
+            }, status.HTTP_202_ACCEPTED)
+            response = self.client.post(url, data, format='multipart', **extra)
+            r = response.json()
+
+            self.assertEqual(r['pkhash'], pkhash)
+            self.assertEqual(r['validated'], False)
+            self.assertEqual(r['description'],
+                             f'http://testserver/media/datamanagers/{r["pkhash"]}/{self.data_description_filename}')
+            self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_add_datamanager_ko(self):
+        data = {'name': 'toto'}
+
+        url = reverse('substrapp:data_manager-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=0.0',
+        }
+        response = self.client.post(url, data, format='multipart', **extra)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_datamanager_no_version(self):
+
+        _, data = self.get_default_datamanager_data()
+
+        url = reverse('substrapp:data_manager-list')
+
+        response = self.client.post(url, data, format='multipart')
+        r = response.json()
+
+        self.assertEqual(r, {'detail': 'A version is required.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_add_datamanager_wrong_version(self):
+
+        _, data = self.get_default_datamanager_data()
+
+        url = reverse('substrapp:data_manager-list')
+        extra = {
+            'HTTP_ACCEPT': 'application/json;version=-1.0',
+        }
+        response = self.client.post(url, data, format='multipart', **extra)
+        r = response.json()
+
+        self.assertEqual(r, {'detail': 'Invalid version in "Accept" header.'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
