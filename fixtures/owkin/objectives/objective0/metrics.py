ad5e84e4a4f5fe83fb52e4abd2a7c60310c51517
Samuel <samuel.lesuffleur@owkin.com>
2019-07-29 15:20:16 +0200

    metrics of objective: use Dockerfile instead of script

--- fixtures/owkin/objectives/objective0/metrics.py
+++ fixtures/owkin/objectives/objective0/metrics.py
@@ -1,8 +1,12 @@
 from sklearn.metrics import recall_score
 
-from substratools import Metrics as MetricsABC
+import substratools as tools
 
 
-class Metrics(MetricsABC):
+class Metrics(tools.Metrics):
     def score(self, y_true, y_pred):
         return recall_score(y_true.argmax(axis=1), y_pred.argmax(axis=1), average='macro')
+
+
+if __name__ == '__main__':
+    tools.metrics.execute(Metrics())
