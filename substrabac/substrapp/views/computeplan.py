a18ce0c6b9922f3322455ca16df2b6f7137b896c
Samuel <samuel.lesuffleur@owkin.com>
2019-07-31 11:40:20 +0200

    handle testtuple and simplify compute plan code
    small fixes too:
    - typo in field inModelsIDs

--- substrabac/substrapp/views/computeplan.py
+++ substrabac/substrapp/views/computeplan.py
@@ -4,70 +4,36 @@ from rest_framework.viewsets import GenericViewSet
 
 from substrapp.serializers import LedgerComputePlanSerializer
 from substrapp.ledger_utils import query_ledger, LedgerError
-from substrapp.views.utils import get_success_create_code, LedgerException
+from substrapp.views.utils import get_success_create_code
 
 
-class ComputePlanViewSet(
-        mixins.CreateModelMixin,
-        mixins.RetrieveModelMixin,
-        mixins.ListModelMixin,
-        GenericViewSet):
+class ComputePlanViewSet(mixins.CreateModelMixin,
+                         GenericViewSet):
 
     serializer_class = LedgerComputePlanSerializer
 
-    def get_queryset(self):
-        return []
+    def create(self, request, *args, **kwargs):
+        # rely on serializer to parse and validate request data
+        serializer = self.get_serializer(data=dict(request.data))
+        serializer.is_valid(raise_exception=True)
 
-    def perform_create(self, serializer):
-        return serializer.save()
+        # get fltask to handle 408 timeout in next invoke ledger request
+        args = serializer.get_args(serializer.validated_data)
+        try:
+            ledger_response = query_ledger(fcn='createComputePlan', args=args)
+        except LedgerError as e:
+            error = {'message': str(e.msg)}
+            return Response(error, status=e.status)
 
-    def commit(self, serializer, fltask):
-        # create on ledger
+        # create compute plan in ledger
+        fltask = ledger_response.get('fltask')
         try:
             data = serializer.create(serializer.validated_data)
         except LedgerError as e:
-            raise LedgerException({'message': str(e.msg), 'fltask': fltask}, e.status)
-        else:
-            return data
+            error = {'message': str(e.msg), 'fltask': fltask}
+            return Response(error, status=e.status)
 
-    def _create(self, request):
-        traintuples = []
-        input_data = dict(request.data)
-        for traintuple in input_data.get('traintuples', []):
-            traintuples.append({
-                'data_manager_key': traintuple.get('data_manager_key'),
-                'train_data_sample_keys': traintuple.get('train_data_sample_keys', []),
-                'traintuple_id': traintuple.get('traintuple_id'),
-                'in_models_ids': traintuple.get('in_model_ids', []),
-                'tag': traintuple.get('tag', ''),
-            })
-        data = {
-            'algo_key': input_data.get('algo_key'),
-            'objective_key': input_data.get('objective_key'),
-            'traintuples': traintuples,
-        }
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-
-        # Get traintuple fltask to handle 408 timeout in invoke_ledger
-        args = serializer.get_args(serializer.validated_data)
-
-        try:
-            data = query_ledger(fcn='createComputePlan', args=args)
-        except LedgerError as e:
-            raise LedgerException({'message': str(e.msg)}, e.status)
-        else:
-            fltask = data.get('fltask')
-            output_data = self.commit(serializer, fltask)
-            return output_data
-
-    def create(self, request, *args, **kwargs):
-        try:
-            data = self._create(request)
-        except LedgerException as e:
-            return Response(e.data, status=e.st)
-        else:
-            headers = self.get_success_headers(data)
-            st = get_success_create_code()
-            return Response(data, status=st, headers=headers)
+        # send successful response
+        headers = self.get_success_headers(data)
+        status = get_success_create_code()
+        return Response(data, status=status, headers=headers)
