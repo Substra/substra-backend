437b8117ca94d687b70ebc526563d37a27330bd1
Samuel <samuel.lesuffleur@owkin.com>
2019-08-06 12:02:20 +0200

    ledger: add retry on MVCC errors when creating tuple

--- substrabac/substrapp/serializers/ledger/traintuple/util.py
+++ substrabac/substrapp/serializers/ledger/traintuple/util.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
 
-from substrapp.ledger_utils import invoke_ledger
+from substrapp.ledger_utils import invoke_ledger, retry_on_mvcc_error
 
 
+@retry_on_mvcc_error(nbtries=3)
 def createLedgerTraintuple(args, sync=False):
     return invoke_ledger(fcn='createTraintuple', args=args, sync=sync)
