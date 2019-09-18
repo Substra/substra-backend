16d4f6eed714484514f2500706f6baebd416fad9
Jérémy Morel <jeremy.morel@owkin.com>
2019-09-18 16:40:07 +0200

    add current node view

--- substrabac/substrapp/urls.py
+++ substrabac/substrapp/urls.py
@@ -8,7 +8,7 @@ from rest_framework.routers import DefaultRouter
 from substrapp.views import ObjectiveViewSet, DataSampleViewSet, DataManagerViewSet, \
     AlgoViewSet, TrainTupleViewSet, TestTupleViewSet, ModelViewSet, TaskViewSet, \
     ComputePlanViewSet, ObjectivePermissionViewSet, AlgoPermissionViewSet, DataManagerPermissionViewSet, \
-    ModelPermissionViewSet, PermissionNodeViewSet
+    ModelPermissionViewSet, NodeViewSet
 
 # Create a router and register our viewsets with it.
 
@@ -26,7 +26,7 @@ router.register(r'traintuple', TrainTupleViewSet, base_name='traintuple')
 router.register(r'testtuple', TestTupleViewSet, base_name='testtuple')
 router.register(r'task', TaskViewSet, base_name='task')
 router.register(r'compute_plan', ComputePlanViewSet, base_name='compute_plan')
-router.register(r'permission_node', PermissionNodeViewSet, base_name='permission_node')
+router.register(r'node', NodeViewSet, base_name='node')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
