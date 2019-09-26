d2fa2b07ff9ae6be4c188b5b75b30d45286804f3
GuillaumeCisco <guillaumecisco@gmail.com>
2019-09-26 17:56:40 +0200

    Make IncomingNodes secret hashed and salted

--- substrabac/node/models.py
+++ substrabac/node/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password, check_password, is_password_usable
 from django.db import models
 
 import secrets
@@ -10,6 +11,22 @@ class Node(models.Model):
     @staticmethod
     def generate_secret():
         return secrets.token_hex(64)
+
+    def set_password(self, raw_secret):
+        self.secret = make_password(raw_secret)
+        self._secret = raw_secret
+
+    def check_password(self, raw_secret):
+        """
+        Return a boolean of whether the raw_password was correct. Handles
+        hashing formats behind the scenes.
+        """
+        def setter(raw_secret):
+            self.set_password(raw_secret)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._secret = None
+            self.save(update_fields=["secret"])
+        return check_password(raw_secret, self.secret, setter)
 
     class Meta:
         abstract = True
