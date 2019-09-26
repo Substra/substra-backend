e6a3a6ebd8087144401d7ba7d4c6d0d4c1122402
Inal DJAFAR <inal.djafar@ulipse.com>
2019-09-26 16:27:33 +0200

    fix tests

--- substrabac/node/apps.py
+++ substrabac/node/apps.py
@@ -1,10 +1,13 @@
 from django.apps import AppConfig
 from substrapp.ledger_utils import invoke_ledger
 
+from django.conf import settings
+
 
 class NodeConfig(AppConfig):
     name = 'node'
 
     def ready(self):
-        # args is set to empty because fabric-sdk-py doesn't allow None args for invoke operations
-        invoke_ledger(fcn='registerNode', args=[''], sync=True)
+        if hasattr(settings, 'LEDGER'):
+            # args is set to empty because fabric-sdk-py doesn't allow None args for invoke operations
+            invoke_ledger(fcn='registerNode', args=[''], sync=True)
