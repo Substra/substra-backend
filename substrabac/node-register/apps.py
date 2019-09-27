bd2c53afa85a35cff495d3025f2d6160b5bd8031
GuillaumeCisco <guillaumecisco@gmail.com>
2019-09-27 10:05:24 +0200

    Add missing files

--- substrabac/node-register/apps.py
+++ substrabac/node-register/apps.py
@@ -1,13 +1,10 @@
 from django.apps import AppConfig
 from substrapp.ledger_utils import invoke_ledger
 
-from django.conf import settings
-
 
 class NodeRegisterConfig(AppConfig):
     name = 'node-register'
 
     def ready(self):
-        if hasattr(settings, 'LEDGER'):
-            # args is set to empty because fabric-sdk-py doesn't allow None args for invoke operations
-            invoke_ledger(fcn='registerNode', args=[''], sync=True)
+        # args is set to empty because fabric-sdk-py doesn't allow None args for invoke operations
+        invoke_ledger(fcn='registerNode', args=[''], sync=True)
