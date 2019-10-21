99618d5db1c167bd859cdeb19477c82192824137
GuillaumeCisco <guillaumecisco@gmail.com>
2019-10-21 10:37:08 +0200

    Update validation password

--- substrabac/users/management/commands/add_user.py
+++ substrabac/users/management/commands/add_user.py
@@ -1,6 +1,8 @@
 import secrets
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
 
@@ -8,14 +10,27 @@ from django.db import IntegrityError
 class Command(BaseCommand):
     help = 'Add user'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.UserModel = get_user_model()
+
     def add_arguments(self, parser):
         parser.add_argument('username')
         parser.add_argument('password', nargs='?', default=secrets.token_hex(8))
 
     def handle(self, *args, **options):
+
+        username = options['username']
+        password = options['password']
+
         try:
-            User.objects.create_user(username=options['username'], password=options['password'])
-        except IntegrityError as e:
-            self.stderr.write(f'User already exists: {str(e)}')
+            validate_password(password, self.UserModel(username=username))
+        except ValidationError as err:
+            self.stderr.write('\n'.join(err.messages))
         else:
-            self.stdout.write(f"password: {options['password']}")
+            try:
+                self.UserModel.objects.create_user(username=username, password=password)
+            except IntegrityError as e:
+                self.stderr.write(f'User already exists: {str(e)}')
+            else:
+                self.stdout.write(f"password: {password}")
