ec4308aaf3b56decb0e4696e7364c2ece3ee56a2
Inal DJAFAR <inal.djafar@ulipse.com>
2019-11-27 18:05:56 +0100

    wip compute plan

--- backend/substrapp/serializers/ledger/computeplan/serializer.py
+++ backend/substrapp/serializers/ledger/computeplan/serializer.py
@@ -5,8 +5,11 @@ from django.conf import settings
 from .util import createLedgerComputePlan
 from .tasks import createLedgerComputePlanAsync
 
+from substrapp.serializers.ledger.utils import PermissionsSerializer
+
 
 class ComputePlanTraintupleSerializer(serializers.Serializer):
+    algo_key = serializers.CharField(min_length=64, max_length=64)
     data_manager_key = serializers.CharField(min_length=64, max_length=64)
     train_data_sample_keys = serializers.ListField(
         child=serializers.CharField(min_length=64, max_length=64),
@@ -20,6 +23,7 @@ class ComputePlanTraintupleSerializer(serializers.Serializer):
 
 
 class ComputePlanTesttupleSerializer(serializers.Serializer):
+    algo_key = serializers.CharField(min_length=64, max_length=64)
     traintuple_id = serializers.CharField(min_length=1, max_length=64)
     data_manager_key = serializers.CharField(min_length=64, max_length=64, required=False)
     test_data_sample_keys = serializers.ListField(
@@ -29,11 +33,34 @@ class ComputePlanTesttupleSerializer(serializers.Serializer):
     tag = serializers.CharField(min_length=0, max_length=64, allow_blank=True, required=False)
 
 
-class LedgerComputePlanSerializer(serializers.Serializer):
+class ComputePlanCompositeTrainTupleSerializer(ComputePlanTraintupleSerializer):
     algo_key = serializers.CharField(min_length=64, max_length=64)
+    data_manager_key = serializers.CharField(min_length=64, max_length=64)
+    train_data_sample_keys = serializers.ListField(
+        child=serializers.CharField(min_length=64, max_length=64),
+        min_length=1)
+    in_head_model_id = serializers.CharField(min_length=64, max_length=64, allow_blank=True, required=False)
+    in_trunk_model_id = serializers.CharField(min_length=64, max_length=64, allow_blank=True, required=False)
+    out_trunk_model_permissions = PermissionsSerializer()
+    tag = serializers.CharField(min_length=0, max_length=64, allow_blank=True, required=False)
+
+
+class ComputePlanAggregatetupleSerializer(serializers.Serializer):
+    algo_key = serializers.CharField(min_length=64, max_length=64)
+    worker = serializers.CharField()
+    in_models_ids = serializers.ListField(
+        child=serializers.CharField(min_length=1, max_length=64),
+        min_length=0,
+        required=False)
+    tag = serializers.CharField(min_length=0, max_length=64, allow_blank=True, required=False)
+
+
+class LedgerComputePlanSerializer(serializers.Serializer):
     objective_key = serializers.CharField(min_length=64, max_length=64)
     traintuples = ComputePlanTraintupleSerializer(many=True)
     testtuples = ComputePlanTesttupleSerializer(many=True)
+    composite_traintuples = ComputePlanCompositeTrainTupleSerializer(many=True)
+    aggregatetuples = ComputePlanAggregatetupleSerializer(many=True)
 
     def get_args(self, data):
         # convert snake case fields to camel case fields to match chaincode expected inputs
@@ -42,6 +69,7 @@ class LedgerComputePlanSerializer(serializers.Serializer):
             traintuple = {
                 'dataManagerKey': data_traintuple['data_manager_key'],
                 'dataSampleKeys': data_traintuple['train_data_sample_keys'],
+                'algoKey': data_traintuple['algo_key'],
                 'id': data_traintuple['traintuple_id'],
             }
             if 'in_models_ids' in data_traintuple:
@@ -54,6 +82,7 @@ class LedgerComputePlanSerializer(serializers.Serializer):
         testtuples = []
         for data_testtuple in data['testtuples']:
             testtuple = {
+                'algoKey': data_traintuple['algo_key'],
                 'traintupleID': data_testtuple['traintuple_id'],
             }
             if 'tag' in data_testtuple:
@@ -65,11 +94,47 @@ class LedgerComputePlanSerializer(serializers.Serializer):
 
             testtuples.append(testtuple)
 
+        composite_traintuples = []
+        for data_composite_traintuple in data['composite_traintuples']:
+            composite_traintuple = {
+                'algoKey': data_composite_traintuple['algo_key'],
+                'dataManagerKey': data_composite_traintuple['data_manager_key'],
+                'dataSampleKeys': data_composite_traintuple['train_data_sample_keys'],
+            }
+
+            if 'tag' in data_composite_traintuple:
+                composite_traintuple['tag'] = data_composite_traintuple['tag']
+            if 'in_head_model_id' in data_composite_traintuple:
+                composite_traintuple['inHeadModelID'] = data_composite_traintuple['in_head_model_id']
+            if 'in_trunk_model_id' in data_composite_traintuple:
+                composite_traintuple['inTrunkModelID'] = data_composite_traintuple['in_trunk_model_id']
+            if 'out_trunk_model_permissions' in data_composite_traintuple:
+                composite_traintuple['outTrunkModelPermissions'] = data_composite_traintuple[
+                    'out_trunk_model_permissions'
+                ]
+
+            composite_traintuples.append(composite_traintuple)
+
+        aggregatetuples = []
+        for data_aggregatetuple in data['aggregatetuples']:
+            aggregatetuple = {
+                'algoKey': data_aggregatetuple['algo_key'],
+                'worker': data_aggregatetuple['worker'],
+            }
+
+            if 'in_models_ids' in data_aggregatetuple:
+                aggregatetuple['inModelsIDs'] = data_aggregatetuple['in_models_ids']
+            if 'tag' in data_aggregatetuple:
+                aggregatetuple['tag'] = data_aggregatetuple['tag']
+
+            aggregatetuples.append(aggregatetuple)
+
         return {
-            'algoKey': data['algo_key'],
             'objectiveKey': data['objective_key'],
             'traintuples': traintuples,
             'testtuples': testtuples,
+            'composite_traintuples': composite_traintuples,
+            'aggregatetuples': aggregatetuples
         }
 
     def create(self, validated_data):
