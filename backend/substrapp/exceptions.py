a7ca5d0a2ecceb62aaa36cbb64f4251787d9ba61
Samuel <samuel.lesuffleur@owkin.com>
2020-01-20 13:21:58 +0100

    improve ledger errors in views
    and apply to composite traintuple view

--- backend/substrapp/exceptions.py
+++ backend/substrapp/exceptions.py
@@ -18,12 +18,17 @@ def api_exception_handler(exc, context):
 
 class _ApiError(Exception):
     """Base error response returned by API."""
-    status = drf.status.HTTP_500_INTERNAL_SERVER_ERROR
-    message = "Internal server error."
+    default_status = drf.status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_message = "Internal server error."
 
-    def __init__(self, message=None, data=None):
-        message = message or self.message
+    def __init__(self, message=None, data=None, status=None):
+        message = message or self.default_message
+        status = status or self.default_status
 
+        assert message
+        assert status
+
+        self.status = status
         self.error = data or {}
         self.error['message'] = message
 
@@ -35,5 +40,24 @@ class _ApiError(Exception):
 
 
 class BadRequestError(_ApiError):
-    status = drf.status.HTTP_400_BAD_REQUEST
-    message = "Bad request."
+    default_status = drf.status.HTTP_400_BAD_REQUEST
+    default_message = "Bad request."
+
+
+class LedgerError(_ApiError):
+    """Forward exception raised from ledger network."""
+    default_status = None  # must be set at init
+    default_message = None  # must be set at init
+
+    def __init__(self, message, status, data=None):
+        super().__init__(message=message, data=data, status=status)
+
+
+def from_ledger_error(exn, data=None):
+    """Returns an API error from a ledger error."""
+    # TODO implement better interface between ledger errors and api errors
+    message = exn.msg
+    data = data or {}
+    if hasattr(exn, 'pkhash') and not data.get('key'):
+        data['key'] = exn.pkhash
+    return LedgerError(f"LedgerError: {message}", exn.status, data=data)
