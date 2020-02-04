6fdb29ea82ac8810d31c0e6d81d9b2ef9cfd7b0d
Kelvin Moutet <kelvin.moutet@owkin.com>
2020-02-04 16:26:20 +0100

    Add head_model test

--- backend/substrapp/tests/query/tests_query_compositetraintuple.py
+++ backend/substrapp/tests/query/tests_query_compositetraintuple.py
@@ -10,10 +10,11 @@ from django.test import override_settings
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from substrapp.models import Objective
-from substrapp.utils import get_hash
+from substrapp.models import Objective, Model
+from substrapp.utils import get_hash, compute_hash
+from node.authentication import NodeUser
 
-from ..common import get_sample_objective, AuthenticatedClient
+from ..common import get_sample_objective, AuthenticatedClient, get_sample_model
 
 MEDIA_ROOT = tempfile.mkdtemp()
 
@@ -33,6 +34,8 @@ class CompositeTraintupleQueryTests(APITestCase):
 
         self.train_data_sample_keys = ['5c1d9cd1c2c1082dde0921b56d11030c81f62fbb51932758b58ac2569dd0b422']
         self.fake_key = '5c1d9cd1c2c1082dde0921b56d11030c81f62fbb51932758b58ac2569dd0a088'
+
+        self.model, _ = get_sample_model()
 
     def tearDown(self):
         shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
@@ -137,3 +140,66 @@ class CompositeTraintupleQueryTests(APITestCase):
         data = {'objective': get_hash(self.objective_description)}
         response = self.client.post(url, data, format='multipart', **extra)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_head_model_ok(self):
+        pkhash = compute_hash(self.model.read(), key='key_traintuple')
+        head_model = Model.objects.create(file=self.model, pkhash=pkhash, validated=True)
+        permissions = {
+            "process": {
+                "public": False,
+                "authorizedIDs": ['substra']
+            }
+        }
+        with mock.patch('substrapp.views.utils.get_owner', return_value='foo'), \
+                mock.patch('substrapp.views.utils.get_object_from_ledger') \
+                as mget_object_from_ledger, \
+                mock.patch('substrapp.views.model.type') as mtype:
+            mget_object_from_ledger.return_value = permissions
+            mtype.return_value = NodeUser
+            extra = {
+                'HTTP_ACCEPT': 'application/json;version=0.0',
+            }
+            response = self.client.get(f'/model/{head_model.pkhash}/file/', **extra)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(head_model.pkhash, compute_hash(response.getvalue(), key='key_traintuple'))
+
+    def test_get_head_model_ko_user(self):
+        pkhash = compute_hash(self.model.read(), key='key_traintuple')
+        head_model = Model.objects.create(file=self.model, pkhash=pkhash, validated=True)
+        permissions = {
+            "process": {
+                "public": False,
+                "authorizedIDs": ['substra']
+            }
+        }
+        with mock.patch('substrapp.views.utils.get_owner', return_value='foo'), \
+                mock.patch('substrapp.views.utils.get_object_from_ledger') \
+                as mget_object_from_ledger:
+            mget_object_from_ledger.return_value = permissions
+            extra = {
+                'HTTP_ACCEPT': 'application/json;version=0.0',
+            }
+            response = self.client.get(f'/model/{head_model.pkhash}/file/', **extra)
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_head_model_ko_wrong_node(self):
+        pkhash = compute_hash(self.model.read(), key='key_traintuple')
+        head_model = Model.objects.create(file=self.model, pkhash=pkhash, validated=True)
+        permissions = {
+            "process": {
+                "public": False,
+                "authorizedIDs": ['owkin']
+            }
+        }
+        with mock.patch('substrapp.views.utils.get_owner', return_value='foo'), \
+                mock.patch('substrapp.views.utils.get_object_from_ledger') \
+                as mget_object_from_ledger, \
+                mock.patch('substrapp.views.model.type') as mtype:
+            mget_object_from_ledger.return_value = permissions
+            mtype.return_value = NodeUser
+
+            extra = {
+                'HTTP_ACCEPT': 'application/json;version=0.0',
+            }
+            response = self.client.get(f'/model/{head_model.pkhash}/file/', **extra)
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
