96a4ffa4d643cd21a729f9a74aefa82cdb5c715a
Jérémy Morel <jeremy.morel@owkin.com>
2020-05-14 09:58:51 +0200

    remove public key from trunk model permissions (#188)

--- backend/substrapp/serializers/ledger/utils.py
+++ backend/substrapp/serializers/ledger/utils.py
@@ -4,3 +4,7 @@ from rest_framework import serializers
 class PermissionsSerializer(serializers.Serializer):
     public = serializers.BooleanField()
     authorized_ids = serializers.ListField(child=serializers.CharField())
+
+
+class PrivatePermissionsSerializer(serializers.Serializer):
+    authorized_ids = serializers.ListField(child=serializers.CharField())
