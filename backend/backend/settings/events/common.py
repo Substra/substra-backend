0fc81019846462e90b4e455dbf0457879f6e07d2
Aurélien Gasser <aurelien.gasser@gmail.com>
2020-09-18 16:03:33 -0400

    remove unnecessary apps

--- backend/backend/settings/events/common.py
+++ backend/backend/settings/events/common.py
@@ -4,14 +4,7 @@ from ..dev import *
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django_celery_results',
-    'rest_framework_swagger',
     'rest_framework',
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt.token_blacklist',
     'events'
 ]
