10e60831e8f6b4315ef0c8134468583e2b2b1599
Aurélien Gasser <aurelien.gasser@gmail.com>
2020-10-27 14:18:22 -0400

    fix migration

--- backend/substrapp/migrations/0004_key_checksum.py
+++ backend/substrapp/migrations/0004_key_checksum.py
@@ -5,8 +5,8 @@ from django.db.models import F
 def get_migration(model_name):
     return [
         migrations.AddField(model_name, 'checksum', models.CharField(blank=True, max_length=64)),
-        migrations.AlterField(model_name, 'key', models.UUIDField(primary_key=True, editable=False)),
-        migrations.RenameField(model_name, 'key', 'key')
+        migrations.AlterField(model_name, 'pkhash', models.UUIDField(primary_key=True, editable=False)),
+        migrations.RenameField(model_name, 'pkhash', 'key')
     ]
 
 
