4fd8f785de8f0be61cdd221b6b25222392452d9c
Matthieu Blottière <matthieu.blottiere@owkin.com>
2021-01-19 12:20:53 +0100

    fix migrations
    manage.py migrate was throwing a warning about models and db being out
    of sync.
    
    For some reason it seems the users_channel.id field was missing from
    the migration, although it was correctly set in DB.

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Channel',
             fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('user', models.OneToOneField(
                     on_delete=django.db.models.deletion.CASCADE,
                     parent_link=True,
