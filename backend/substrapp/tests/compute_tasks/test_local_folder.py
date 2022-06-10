b66ca34bc938dfa70dbb2dc1c4de546ee78d886d
Alexandre-P <alexandre.picosson@owkin.com>
2022-06-10 14:21:23 +0200

    test: update local folders tests to use the orchestrator mock (#1100)

--- backend/substrapp/tests/compute_tasks/test_local_folder.py
+++ backend/substrapp/tests/compute_tasks/test_local_folder.py
@@ -1,110 +1,92 @@
 import os
 import tempfile
-import uuid
-from unittest import mock
+from contextlib import nullcontext
 
-from django.test import override_settings
-from parameterized import parameterized
-from rest_framework.test import APITestCase
+import pytest
+from pytest_mock import MockerFixture
 
 import orchestrator.computetask_pb2 as computetask_pb2
+from substrapp.compute_tasks.context import Context
 from substrapp.compute_tasks.directories import CPDirName
 from substrapp.compute_tasks.directories import TaskDirName
 from substrapp.tasks.tasks_compute_task import compute_task
+from substrapp.tests.orchestrator_factory import Orchestrator
 
 CHANNEL = "mychannel"
 
 
-@override_settings(LEDGER_CHANNELS={CHANNEL: {"chaincode": {"name": "mycc"}, "model_export_enabled": True}})
-class LocalFolderTests(APITestCase):
-    @parameterized.expand([("without_exception", False), ("with_exception", True)])
-    def test_local_folder(self, _, compute_job_raises):
-        """
-        This test ensures that changes to the subtuple local folder are reflected to the compute plan local folder iff
-        the tuple execution succeeds.
-        """
+@pytest.mark.parametrize("text, compute_job_raises", [("with_exception", False), ("with_exception", True)])
+def test_local_folder(text: str, compute_job_raises: bool, settings, mocker: MockerFixture, orchestrator: Orchestrator):
+    """
+    This test ensures that changes to the subtuple local folder are reflected to the compute plan local folder iff
+    the tuple execution succeeds.
+    """
+    del text
+    settings.LEDGER_CHANNELS = {CHANNEL: {"chaincode": {"name": "mycc"}, "model_export_enabled": True}}
+    mocker.patch("substrapp.compute_tasks.context.get_orchestrator_client", return_value=orchestrator.client)
+    mocker.patch("substrapp.tasks.tasks_compute_task.get_orchestrator_client", return_value=orchestrator.client)
+    mocker.patch("substrapp.tasks.tasks_compute_task.init_asset_buffer")
+    mocker.patch("substrapp.tasks.tasks_compute_task.init_compute_plan_dirs")
+    mocker.patch("substrapp.tasks.tasks_compute_task.build_images")
+    mocker.patch("substrapp.tasks.tasks_compute_task.add_task_assets_to_buffer")
+    mocker.patch("substrapp.tasks.tasks_compute_task.add_assets_to_taskdir")
+    mock_execute_compute_task = mocker.patch("substrapp.tasks.tasks_compute_task.execute_compute_task")
+    mocker.patch("substrapp.tasks.tasks_compute_task.save_models")
+    mocker.patch("substrapp.tasks.tasks_compute_task.teardown_task_dirs")
 
-        compute_plan_key = str(uuid.uuid4())
-        file = "model.txt"
-        initial_value = "initial value"
-        updated_value = "updated value"
+    file = "model.txt"
+    initial_value = "initial value"
+    updated_value = "updated value"
 
-        task = {
-            "key": str(uuid.uuid4()),
-            "compute_plan_key": compute_plan_key,
-            "rank": 1,
-            "algo": {"key": "some key", "checksum": "some checksum"},
-            "category": computetask_pb2.ComputeTaskCategory.Name(computetask_pb2.TASK_TRAIN),
-        }
+    train_task = orchestrator.create_train_task(status=computetask_pb2.STATUS_DOING)
 
-        class FakeDirectories:
-            compute_plan_dir = tempfile.mkdtemp()
-            task_dir = tempfile.mkdtemp()
+    # Setup a fake context
+    task = orchestrator.client.query_task(train_task.key)
+    ctx = Context.from_task(CHANNEL, task)
 
-        class FakeContext:
-            directories = FakeDirectories()
-            compute_plan_key = "some compute plan key"
-            task_category = computetask_pb2.ComputeTaskCategory.Name(computetask_pb2.TASK_TRAIN)
-            has_chainkeys = False
-            all_algos = []
+    class FakeDirectories:
+        compute_plan_dir = tempfile.mkdtemp()
+        task_dir = tempfile.mkdtemp()
 
-        ctx = FakeContext()
+    ctx._directories = FakeDirectories()
+    ctx._has_chainkeys = False
+    mock_ctx_from_task = mocker.patch("substrapp.tasks.tasks_compute_task.Context.from_task", return_value=ctx)
 
-        local_folder = os.path.join(ctx.directories.task_dir, TaskDirName.Local)
-        local_folder_committed = os.path.join(ctx.directories.compute_plan_dir, CPDirName.Local)
+    local_folder = os.path.join(ctx.directories.task_dir, TaskDirName.Local)
+    local_folder_committed = os.path.join(ctx.directories.compute_plan_dir, CPDirName.Local)
 
-        # Write an initial value into the compute plan local folder
-        os.makedirs(local_folder, exist_ok=True)
-        os.makedirs(local_folder_committed, exist_ok=True)
-        with open(os.path.join(local_folder_committed, file), "w") as x:
-            x.write(initial_value)
+    # Write an initial value into the compute plan local folder
+    os.makedirs(local_folder_committed, exist_ok=True)
+    with open(os.path.join(local_folder_committed, file), "w") as x:
+        x.write(initial_value)
 
-        with mock.patch(
-            "substrapp.compute_tasks.compute_task.raise_if_task_not_runnable"
-        ) as mraise_if_task_not_runnable, mock.patch(
-            "substrapp.tasks.tasks_compute_task.Context.from_task"
-        ) as mfrom_task, mock.patch(
-            "substrapp.tasks.tasks_compute_task.init_asset_buffer"
-        ), mock.patch(
-            "substrapp.tasks.tasks_compute_task.add_task_assets_to_buffer"
-        ), mock.patch(
-            "substrapp.tasks.tasks_compute_task.add_assets_to_taskdir"
-        ), mock.patch(
-            "substrapp.tasks.tasks_compute_task.execute_compute_task"
-        ) as mexecute_compute_task, mock.patch(
-            "substrapp.tasks.tasks_compute_task.save_models"
-        ), mock.patch(
-            "substrapp.tasks.tasks_compute_task.teardown_task_dirs"
-        ):
+    def execute(*args, **kwargs):
+        del args, kwargs
+        nonlocal local_folder
+        with open(os.path.join(local_folder, file), "w") as x:
+            x.write(updated_value)
+            print("test")
+        if compute_job_raises:
+            raise Exception("I'm an error")
 
-            mraise_if_task_not_runnable.return_value = True
-            mfrom_task.return_value = FakeContext
+    mock_execute_compute_task.side_effect = execute
 
-            # The function `execute_compute_task` will:
-            # 1. write a new value to the subtuple local folder
-            # 2. and then:
-            #    - complete successfully (compute_job_raises == False)
-            #    - or raise an exception (compute_job_raises == True)
+    # assert that it raises when it should
+    context = pytest.raises(Exception) if compute_job_raises else nullcontext()
+    with context as excinfo:
+        compute_task(CHANNEL, task, train_task.compute_plan_key)
 
-            def execute(*args, **kwargs):
-                nonlocal local_folder
-                with open(os.path.join(local_folder, file), "w") as x:
-                    x.write(updated_value)
-                if compute_job_raises:
-                    raise Exception("I'm an error")
+    # Check the compute plan local folder value is correct:
+    # - If do_task did raise an exception then the local value should be unchanged
+    # - If do_task did not raise an exception then the local value should be updated
+    with open(os.path.join(local_folder_committed, file), "r") as x:
+        content = x.read()
+    expected = initial_value if compute_job_raises else updated_value
+    assert content == expected
 
-            mexecute_compute_task.side_effect = execute
+    mock_execute_compute_task.assert_called_once()
+    mock_ctx_from_task.assert_called_once()
 
-            try:
-                compute_task(CHANNEL, task, compute_plan_key)
-            except Exception:
-                if compute_job_raises:
-                    # exception expected
-                    pass
-
-        # Check the compute plan local folder value is correct:
-        # - If do_task did raise an exception then the local value should be unchanged
-        # - If do_task did not raise an exception then the local value should be updated
-        with open(os.path.join(local_folder_committed, file), "r") as x:
-            content = x.read()
-        self.assertEqual(content, initial_value if compute_job_raises else updated_value)
+    # check that it was the expected exception that was raised
+    if compute_job_raises:
+        assert "I'm an error" in str(excinfo.value.__cause__)
