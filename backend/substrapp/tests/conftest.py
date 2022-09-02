f3c7455e4dd1a0d86f75124038566afbf043d449
Matthieu Blottière <matthieu.blottiere@owkin.com>
2022-09-02 11:11:18 +0200

    refactor: remove unused orchestrator_factory mocks
    Signed-off-by: Matthieu Blottière <matthieu.blottiere@owkin.com>

--- backend/substrapp/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import substrapp.tests.orchestrator_factory
-
-
-@pytest.fixture
-def orchestrator() -> substrapp.tests.orchestrator_factory.Orchestrator:
-    return substrapp.tests.orchestrator_factory.Orchestrator()
