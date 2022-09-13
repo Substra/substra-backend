7a3ffbfda7530ce932feb8f6b372210285a38c79
Kelvin Moutet <kelvin.moutet@owkin.com>
2022-09-13 15:35:41 +0200

    fix: change INSTALLED_APPS order for management commands
    Signed-off-by: Kelvin Moutet <kelvin.moutet@owkin.com>

--- backend/backend/settings/api/events/common.py
+++ backend/backend/settings/api/events/common.py
@@ -2,6 +2,9 @@ from ...deps.ledger import *
 from ...deps.orchestrator import *
 from ...dev import *
 
+# order is important for api and substrapp because of overriding commands
+# https://docs.djangoproject.com/en/4.1/howto/custom-management-commands/#overriding-commands
+
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
