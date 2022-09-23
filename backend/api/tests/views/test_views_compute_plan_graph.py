b4789b55c24dd5c937f826b76ac3fff93e8a987e
alexisdeh <87314184+alexisdeh@users.noreply.github.com>
2022-09-23 15:30:47 +0200

    Update backend/api/tests/views/test_views_compute_plan_graph.py
    Co-authored-by: Matthieu Blottière <49146345+mblottiere@users.noreply.github.com>
    Signed-off-by: Alexis de Hauteclocque <alexis.dehauteclocque@owkin.com>

--- backend/api/tests/views/test_views_compute_plan_graph.py
+++ backend/api/tests/views/test_views_compute_plan_graph.py
@@ -72,7 +72,6 @@ class ComputePlanGraphViewTests(APITestCase):
             compute_plan, algo=algo, category=ComputeTask.Category.TASK_AGGREGATE, parent_tasks=[composite_task.key]
         )
 
-        self.maxDiff = None
         expected_results = {
             "tasks": [
                 {
