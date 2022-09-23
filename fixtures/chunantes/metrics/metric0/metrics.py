e4643f52d62e83a5539996a6309735ce4e251ced
Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
2022-09-23 15:00:04 +0200

    feat: update following the metric change in substratools (#472)
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- fixtures/chunantes/metrics/metric0/metrics.py
+++ fixtures/chunantes/metrics/metric0/metrics.py
@@ -2,10 +2,10 @@ import substratools as tools
 from sklearn.metrics import recall_score
 
 
-class Metrics(tools.Metrics):
+class Metrics(tools.MetricAlgo):
     def score(self, y_true, y_pred):
         return recall_score(y_true.argmax(axis=1), y_pred.argmax(axis=1), average="macro")
 
 
 if __name__ == "__main__":
-    tools.metrics.execute(Metrics())
+    tools.algo.execute(Metrics())
