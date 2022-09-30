f2f8f19eca04df8f63646b5001de18c66e61f2f4
Serge Bouchut <serge.bouchut@owkin.com>
2022-09-30 11:06:22 +0200

    chore: improve test data with real cp topologies
    Signed-off-by: Serge Bouchut <serge.bouchut@owkin.com>

--- backend/api/management/commands/generate_fixtures.py
+++ backend/api/management/commands/generate_fixtures.py
@@ -1,218 +1,297 @@
-from django.core.files.base import ContentFile as _
+import structlog
+from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
 
 from api.models import ComputePlan
 from api.models import ComputeTask
 from api.tests import asset_factory as factory
 
+logger = structlog.get_logger(__name__)
+
 
 class Command(BaseCommand):
     help = "Generate fixtures"
 
     def handle(self, *args, **options):
-        simple_algo = factory.create_algo(
+        logger.debug("Generate test data")
+        self.create_algos()
+        self.create_data_manager()
+        self.create_data_samples()
+        self.create_empty_cp()
+        for status in [
+            ComputePlan.Status.PLAN_STATUS_TODO,
+            ComputePlan.Status.PLAN_STATUS_DOING,
+            ComputePlan.Status.PLAN_STATUS_DONE,
+            ComputePlan.Status.PLAN_STATUS_FAILED,
+            ComputePlan.Status.PLAN_STATUS_CANCELED,
+        ]:
+            self.create_basic_cp(status)
+        self.create_aggregate_cp()
+        self.create_composite_cp()
+
+    def create_algos(self):
+        logger.debug("  Create algos")
+        self.simple_algo = factory.create_algo(
             inputs=factory.build_algo_inputs(["datasamples", "opener", "model"]),
             outputs=factory.build_algo_outputs(["model"]),
-            name="simple algo",
+            name="simple",
         )
-        aggregate_algo = factory.create_algo(
+        factory.create_algo_files(
+            key=self.simple_algo.key,
+            description=ContentFile("Simple algo"),
+        )
+
+        self.aggregate_algo = factory.create_algo(
             inputs=factory.build_algo_inputs(["model"]),
             outputs=factory.build_algo_outputs(["model"]),
             name="aggregate",
         )
-        composite_algo = factory.create_algo(
+        factory.create_algo_files(
+            key=self.aggregate_algo.key,
+            description=ContentFile("Aggregate algo"),
+        )
+
+        self.composite_algo = factory.create_algo(
             inputs=factory.build_algo_inputs(["datasamples", "opener", "local", "shared"]),
             outputs=factory.build_algo_outputs(["local", "shared"]),
             name="composite",
         )
-        metric_algo = factory.create_algo(
+        factory.create_algo_files(
+            key=self.composite_algo.key,
+            description=ContentFile("Composite algo"),
+        )
+
+        self.predict_algo = factory.create_algo(
+            inputs=factory.build_algo_inputs(["datasamples", "opener", "model", "shared"]),
+            outputs=factory.build_algo_outputs(["predictions"]),
+            name="predict",
+        )
+        factory.create_algo_files(
+            key=self.predict_algo.key,
+            description=ContentFile("Predict algo"),
+        )
+
+        self.metric_algo = factory.create_algo(
             inputs=factory.build_algo_inputs(["datasamples", "opener", "predictions"]),
             outputs=factory.build_algo_outputs(["performance"]),
             name="metric",
         )
+        factory.create_algo_files(
+            key=self.metric_algo.key,
+            description=ContentFile("Metric algo"),
+        )
 
-        data_manager = factory.create_datamanager()
-        train_data_sample = factory.create_datasample([data_manager])
-        test_data_sample = factory.create_datasample([data_manager], test_only=True)
+    def create_data_manager(self):
+        logger.debug("  Create data manager")
+        self.data_manager = factory.create_datamanager()
+        factory.create_datamanager_files(key=self.data_manager.key)
 
-        # CP without task
-        factory.create_computeplan(
-            name="CP without task",
+    def create_data_samples(self):
+        logger.debug("  Create data samples")
+        self.train_data_sample_keys = []
+        self.predict_data_sample_keys = []
+        self.test_data_sample_keys = []
+
+        for _ in range(3):
+            train_data_sample = factory.create_datasample([self.data_manager])
+            factory.create_datasample_files(key=train_data_sample.key)
+            self.train_data_sample_keys.append(train_data_sample.key)
+
+            predict_data_sample = factory.create_datasample([self.data_manager])
+            factory.create_datasample_files(key=predict_data_sample.key)
+            self.predict_data_sample_keys.append(predict_data_sample.key)
+
+            test_data_sample = factory.create_datasample([self.data_manager])
+            factory.create_datasample_files(key=test_data_sample.key)
+            self.test_data_sample_keys.append(test_data_sample.key)
+
+    def create_empty_cp(self):
+        logger.debug("  Create empty CP")
+        # no task
+        return factory.create_computeplan(
+            name="empty",
             status=ComputePlan.Status.PLAN_STATUS_EMPTY,
+        )
+
+    def create_basic_cp(self, cp_status):
+        logger.debug(f"  Create basic CP [{cp_status}]")
+        # train -> predict -> test
+        cp = factory.create_computeplan(
+            name=f"basic [{cp_status}]",
+            status=cp_status,
             metadata={
-                "device": "cpu",
-                "epochs": "30",
-                "lr_steps": "2 10",
-                "optimizer": "Adam",
-                "dataset_name": "reg_phase1_new",
+                "status": cp_status,
             },
         )
 
-        # CP with a single todo train task
-        todo_cp = factory.create_computeplan(
-            name="CP with a single todo train task",
-            status=ComputePlan.Status.PLAN_STATUS_TODO,
-            metadata={
-                "device": "cpu",
-                "epochs": "50",
-                "lr_steps": "2 5",
-                "optimizer": "Adam",
-                "dataset_name": "hyb_phase1",
-                "dropouts_reg": "0.6",
-                "regression_weight": "0.75",
-            },
+        first_task_status = cp_status
+        train_task = factory.create_computetask(
+            cp,
+            self.simple_algo,
+            inputs=factory.build_computetask_inputs(
+                self.simple_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.train_data_sample_keys,
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.simple_algo),
+            data_manager=self.data_manager,
+            data_samples=self.train_data_sample_keys,
+            category=ComputeTask.Category.TASK_TRAIN,
+            status=first_task_status,
         )
-        train_input_keys = {
-            "opener": [data_manager.key],
-            "datasamples": [train_data_sample.key],
-        }
-        factory.create_computetask(
-            todo_cp,
-            simple_algo,
-            inputs=factory.build_computetask_inputs(simple_algo, train_input_keys),
-            outputs=factory.build_computetask_outputs(simple_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
+        if first_task_status == ComputeTask.Status.STATUS_DONE:
+            model = factory.create_model(
+                train_task,
+                identifier="model",
+            )
+            factory.create_model_files(key=model.key)
+        elif first_task_status == ComputeTask.Status.STATUS_FAILED:
+            factory.create_computetask_logs(compute_task_key=train_task.key)
+            cp.failed_task_key = str(train_task.key)
+            cp.failed_task_category = train_task.category
+            cp.save()
+
+        task_status = (
+            ComputeTask.Status.STATUS_DONE
+            if cp_status == ComputePlan.Status.PLAN_STATUS_DONE
+            else ComputeTask.Status.STATUS_TODO
+        )
+        predict_task = factory.create_computetask(
+            cp,
+            self.predict_algo,
+            inputs=factory.build_computetask_inputs(
+                self.predict_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.predict_data_sample_keys,
+                    "model": [train_task.key],
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.predict_algo),
+            data_manager=self.data_manager,
+            data_samples=self.predict_data_sample_keys,
+            category=ComputeTask.Category.TASK_PREDICT,
+            status=task_status,
+        )
+        if task_status == ComputeTask.Status.STATUS_DONE:
+            predictions = factory.create_model(
+                predict_task,
+                identifier="predictions",
+            )
+            factory.create_model_files(key=predictions.key)
+
+        test_task = factory.create_computetask(
+            cp,
+            self.metric_algo,
+            inputs=factory.build_computetask_inputs(
+                self.metric_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.test_data_sample_keys,
+                    "predictions": [predict_task.key],
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.metric_algo),
+            data_manager=self.data_manager,
+            data_samples=self.test_data_sample_keys,
+            category=ComputeTask.Category.TASK_TEST,
+            status=task_status,
+        )
+        if task_status == ComputeTask.Status.STATUS_DONE:
+            factory.create_performance(
+                test_task,
+                self.metric_algo,
+                identifier="performance",
+            )
+
+        return cp
+
+    def create_aggregate_cp(self):
+        logger.debug("  Create aggregate CP")
+        # (train, train) -> aggregate
+        cp = factory.create_computeplan(
+            name="aggregate",
+            status=ComputePlan.Status.PLAN_STATUS_TODO,
+        )
+        train_task_1 = factory.create_computetask(
+            cp,
+            self.simple_algo,
+            inputs=factory.build_computetask_inputs(
+                self.simple_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.train_data_sample_keys,
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.simple_algo),
+            data_manager=self.data_manager,
+            data_samples=self.train_data_sample_keys,
             category=ComputeTask.Category.TASK_TRAIN,
             status=ComputeTask.Status.STATUS_TODO,
         )
-
-        # CP with two done train tasks and a doing aggregate train task
-        doing_cp = factory.create_computeplan(
-            name="CP with two done train tasks and a doing aggregate train task",
-            status=ComputePlan.Status.PLAN_STATUS_DOING,
-        )
-        train_task_1 = factory.create_computetask(
-            doing_cp,
-            simple_algo,
-            inputs=factory.build_computetask_inputs(simple_algo, train_input_keys),
-            outputs=factory.build_computetask_outputs(simple_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
-            category=ComputeTask.Category.TASK_TRAIN,
-            status=ComputeTask.Status.STATUS_DONE,
-        )
-        model_1 = factory.create_model(train_task_1, identifier="model")
         train_task_2 = factory.create_computetask(
-            doing_cp,
-            simple_algo,
-            inputs=factory.build_computetask_inputs(simple_algo, train_input_keys),
-            outputs=factory.build_computetask_outputs(simple_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
+            cp,
+            self.simple_algo,
+            inputs=factory.build_computetask_inputs(
+                self.simple_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.train_data_sample_keys,
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.simple_algo),
+            data_manager=self.data_manager,
+            data_samples=self.train_data_sample_keys,
             category=ComputeTask.Category.TASK_TRAIN,
-            status=ComputeTask.Status.STATUS_DONE,
+            status=ComputeTask.Status.STATUS_TODO,
         )
-        model_2 = factory.create_model(train_task_2, identifier="model")
-        aggregate_input_keys = {
-            "model": [train_task_1.key, train_task_2.key],
-        }
         factory.create_computetask(
-            doing_cp,
-            aggregate_algo,
-            inputs=factory.build_computetask_inputs(aggregate_algo, aggregate_input_keys),
-            outputs=factory.build_computetask_outputs(aggregate_algo),
-            parent_tasks=[train_task_1.key, train_task_2.key],
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
-            category=ComputeTask.Category.TASK_TRAIN,
-            status=ComputeTask.Status.STATUS_DOING,
+            cp,
+            self.aggregate_algo,
+            inputs=factory.build_computetask_inputs(
+                self.aggregate_algo,
+                {
+                    "model": [train_task_1.key, train_task_2.key],
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.aggregate_algo),
+            category=ComputeTask.Category.TASK_AGGREGATE,
+            status=ComputeTask.Status.STATUS_TODO,
         )
+        return cp
 
-        # CP with a done train task and a done test task
-        done_cp = factory.create_computeplan(
-            name="CP with a done train task and a done test task",
+    def create_composite_cp(self):
+        logger.debug("  Create composite CP")
+        cp = factory.create_computeplan(
+            name="composite",
             status=ComputePlan.Status.PLAN_STATUS_DONE,
         )
-        train_task = factory.create_computetask(
-            done_cp,
-            simple_algo,
-            inputs=factory.build_computetask_inputs(simple_algo, train_input_keys),
-            outputs=factory.build_computetask_outputs(simple_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
-            category=ComputeTask.Category.TASK_TRAIN,
-            status=ComputeTask.Status.STATUS_DONE,
-        )
-        model_3 = factory.create_model(train_task, identifier="model")
-        test_input_keys = {
-            "opener": [data_manager.key],
-            "datasamples": [test_data_sample.key],
-        }
-        test_task = factory.create_computetask(
-            done_cp,
-            metric_algo,
-            inputs=factory.build_computetask_inputs(metric_algo, test_input_keys),
-            outputs=factory.build_computetask_outputs(metric_algo),
-            parent_tasks=[train_task.key],
-            data_manager=data_manager,
-            data_samples=[test_data_sample.key],
-            category=ComputeTask.Category.TASK_TEST,
-            status=ComputeTask.Status.STATUS_DONE,
-        )
-        factory.create_performance(test_task, metric_algo)
-
-        # CP with a done composite train task and a failed test task
-        failed_cp = factory.create_computeplan(
-            name="CP with a done composite train task and a failed test task",
-            status=ComputePlan.Status.PLAN_STATUS_FAILED,
-        )
-        composite_input_keys = {
-            "opener": [data_manager.key],
-            "datasamples": [train_data_sample.key],
-        }
         composite_task = factory.create_computetask(
-            failed_cp,
-            composite_algo,
-            inputs=factory.build_computetask_inputs(composite_algo, composite_input_keys),
-            outputs=factory.build_computetask_outputs(composite_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
+            cp,
+            self.composite_algo,
+            inputs=factory.build_computetask_inputs(
+                self.composite_algo,
+                {
+                    "opener": [self.data_manager.key],
+                    "datasamples": self.train_data_sample_keys,
+                },
+            ),
+            outputs=factory.build_computetask_outputs(self.composite_algo),
+            data_manager=self.data_manager,
+            data_samples=self.train_data_sample_keys,
             category=ComputeTask.Category.TASK_COMPOSITE,
             status=ComputeTask.Status.STATUS_DONE,
         )
-        model_4 = factory.create_model(composite_task, identifier="local")
-        failed_task = factory.create_computetask(
-            failed_cp,
-            metric_algo,
-            inputs=factory.build_computetask_inputs(metric_algo, test_input_keys),
-            outputs=factory.build_computetask_outputs(metric_algo),
-            parent_tasks=[composite_task.key],
-            data_manager=data_manager,
-            data_samples=[test_data_sample.key],
-            category=ComputeTask.Category.TASK_TEST,
-            status=ComputeTask.Status.STATUS_FAILED,
+        local_model = factory.create_model(
+            composite_task,
+            identifier="local",
         )
-        failed_cp.failed_task_key = str(failed_task.key)
-        failed_cp.failed_task_category = failed_task.category
-        failed_cp.save()
-
-        # CP with a single canceled train task
-        canceled_cp = factory.create_computeplan(
-            name="CP with a single canceled train task",
-            status=ComputePlan.Status.PLAN_STATUS_CANCELED,
+        factory.create_model_files(key=local_model.key)
+        shared_model = factory.create_model(
+            composite_task,
+            identifier="shared",
         )
-        factory.create_computetask(
-            canceled_cp,
-            simple_algo,
-            inputs=factory.build_computetask_inputs(simple_algo, train_input_keys),
-            outputs=factory.build_computetask_outputs(simple_algo),
-            data_manager=data_manager,
-            data_samples=[train_data_sample.key],
-            category=ComputeTask.Category.TASK_TRAIN,
-            status=ComputeTask.Status.STATUS_CANCELED,
-        )
-
-        # Create files
-        factory.create_algo_files(key=simple_algo.key, description=_("Simple Algo"))
-        factory.create_algo_files(key=aggregate_algo.key, description=_("Aggregate Algo"))
-        factory.create_algo_files(key=composite_algo.key, description=_("Composite Algo"))
-        factory.create_algo_files(key=metric_algo.key, description=_("Metric Algo"))
-        factory.create_datamanager_files(key=data_manager.key)
-        factory.create_datasample_files(key=train_data_sample.key)
-        factory.create_datasample_files(key=test_data_sample.key)
-        factory.create_model_files(key=model_1.key)
-        factory.create_model_files(key=model_2.key)
-        factory.create_model_files(key=model_3.key)
-        factory.create_model_files(key=model_4.key)
-        factory.create_computetask_logs(compute_task_key=failed_task.key)
+        factory.create_model_files(key=shared_model.key)
+        return cp
