465a0c4e59bde3634e86fdbd67652641febe51d0
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-01-04 14:08:35 +0100

    black
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- backend/api/migrations/0041_remove_datasample_test_only.py
+++ backend/api/migrations/0041_remove_datasample_test_only.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0040_alter_computeplan_failed_task_category_and_more'),
+        ("api", "0040_alter_computeplan_failed_task_category_and_more"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='datasample',
-            name='test_only',
+            model_name="datasample",
+            name="test_only",
         ),
     ]
