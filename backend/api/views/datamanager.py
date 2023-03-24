b2b8d7e65dda4d5ef885ef43d68bd0ee3bb38476
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-03-24 16:06:04 +0100

    fix: remove datasample manager
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- backend/api/views/datamanager.py
+++ backend/api/views/datamanager.py
@@ -14,7 +14,6 @@ from rest_framework.viewsets import GenericViewSet
 
 from api.errors import AlreadyExistsError
 from api.models import DataManager
-from api.models import DataSample
 from api.serializers import DataManagerSerializer
 from api.serializers import DataManagerWithRelationsSerializer
 from api.views.filters_utils import CharInFilter
@@ -173,12 +172,7 @@ class DataManagerViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, mixin
 
     def get_queryset(self):
         if self.action in ["list", "retrieve"]:
-            # Data_samples is ordered by  with the custom ObjectManager
-            # (https://github.com/Substra/substra-backend/blob/32240fcb4e06b3183b59e48bfc59a215794fdab5/backend/api/models/datasample.py#L6)
-            # It is  adding a costly join on TaskDataSample to get the order field
-            return DataManager.objects.filter(channel=get_channel_name(self.request)).prefetch_related(
-                models.Prefetch("data_samples", queryset=DataSample.objects_ordered_by_key.distinct())
-            )
+            return DataManager.objects.filter(channel=get_channel_name(self.request)).prefetch_related("data_samples")
         else:
             return DataManager.objects.filter(channel=get_channel_name(self.request))
 
