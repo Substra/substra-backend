e8b8980fd8e2a4ea3cfa6088ca0fac48dd1c7001
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-04-20 17:17:46 +0200

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- backend/api/models/performance.py
+++ backend/api/models/performance.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Performance(models.Model):
-    compute_task = models.ForeignKey("ComputeTask", on_delete=models.deletion.DO_NOTHING, related_name="performances")
     metric = models.ForeignKey("Function", on_delete=models.deletion.DO_NOTHING, related_name="performances")
-    identifier = models.CharField(max_length=100)
+    compute_task_output = models.ForeignKey(
+        "ComputeTaskOutput", on_delete=models.deletion.DO_NOTHING, related_name="performances"
+    )
     value = models.FloatField()
     creation_date = models.DateTimeField()
     channel = models.CharField(max_length=100)
 
     class Meta:
-        unique_together = (("compute_task", "metric", "identifier"),)
+        unique_together = (("compute_task_output", "metric"),)
