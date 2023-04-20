173c7fdcc8eb61ea54db53cada7dbca744ffd2b1
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-04-20 17:56:07 +0200

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- backend/api/models/performance.py
+++ backend/api/models/performance.py
@@ -3,9 +3,7 @@ from django.db import models
 
 class Performance(models.Model):
     metric = models.ForeignKey("Function", on_delete=models.deletion.DO_NOTHING, related_name="performances")
-    compute_task_output_identifier = models.ForeignKey(
-        "ComputeTaskOutput.identifier", on_delete=models.deletion.DO_NOTHING, related_name="performances"
-    )
+    compute_task_output_identifier = models.CharField(max_length=100)
     value = models.FloatField()
     creation_date = models.DateTimeField()
     channel = models.CharField(max_length=100)
