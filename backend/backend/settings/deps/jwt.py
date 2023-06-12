961affff8b21c429e862e33ca6ee668054c0947d
Olivier Léobal <olivier.leobal@owkin.com>
2023-06-12 18:26:55 +0200

    rename SECRET_JEY to JWT_SECRET_KEY
    Signed-off-by: Olivier Léobal <olivier.leobal@owkin.com>

--- backend/backend/settings/deps/jwt.py
+++ backend/backend/settings/deps/jwt.py
@@ -10,13 +10,13 @@ JWT_SECRET_PATH = os.environ.get("JWT_SECRET_PATH", os.path.normpath(os.path.joi
 # Key configuration for JSON web tokens (JWT) authentication
 if common.to_bool(os.environ.get("JWT_SECRET_NEEDED", "False")):
     try:
-        SECRET_KEY = pathlib.Path(JWT_SECRET_PATH).read_text().strip()
+        JWT_SECRET_KEY = pathlib.Path(JWT_SECRET_PATH).read_text().strip()
     except IOError:
         try:
-            SECRET_KEY = secrets.token_urlsafe()  # uses a "reasonable default" length
+            JWT_SECRET_KEY = secrets.token_urlsafe()  # uses a "reasonable default" length
             with open(JWT_SECRET_PATH, "w") as fp:
-                fp.write(SECRET_KEY)
+                fp.write(JWT_SECRET_KEY)
         except IOError:
             raise Exception(f"Cannot open file `{JWT_SECRET_PATH}` for writing.")
 else:
-    SECRET_KEY = "unused default value " + secrets.token_urlsafe()
+    JWT_SECRET_KEY = "unused default value " + secrets.token_urlsafe()
