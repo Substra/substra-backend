0d485d08a333a2a8bdc15acc25fa2c85251721e7
Olivier Léobal <olivier.leobal@owkin.com>
2023-06-22 17:14:04 +0200

    fix: settings.BASE_DIR location
    Signed-off-by: Olivier Léobal <olivier.leobal@owkin.com>

--- backend/backend/settings/deps/path.py
+++ backend/backend/settings/deps/path.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
 sys.path.append(PROJECT_ROOT)
