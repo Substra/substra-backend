9524c2fdb5fc67fce340a072851f0f6476ec0411
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-07-19 16:47:04 +0200

    feat: build image in new pod
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- backend/builder/tasks/tasks_build_image.py
+++ backend/builder/tasks/tasks_build_image.py
@@ -1,5 +1,6 @@
 import orchestrator
 from backend.celery import app
+from builder.image_builder.image_builder import build_image_if_missing
 
 
 @app.task(
@@ -11,6 +12,6 @@ from backend.celery import app
 # Ack late and reject on worker lost allows use to
 # see http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-reject-on-worker-lost
 # and https://github.com/celery/celery/issues/5106
-def build_image(function_serialized: str):
+def build_image(self, function_serialized: str, channel: str):
     function = orchestrator.Function.parse_raw(function_serialized)
-    print(f"youpi!!!! {function.key}")
+    build_image_if_missing(channel, function)
