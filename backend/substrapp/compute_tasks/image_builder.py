99f79c1fceae602b4241fd76c830dc15554426de
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-07-19 16:46:38 +0200

    fix: improve `wait_for_image_built`
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- backend/substrapp/compute_tasks/image_builder.py
+++ backend/substrapp/compute_tasks/image_builder.py
@@ -13,22 +13,19 @@ MAX_IMAGE_BUILD_TIME = 3 * 60 * 60  # 3 hours
 WAITING_TIME = 5  # wait 5 seconds between two queries
 
 
-def wait_for_image_built(function: orchestrator.Function):
+def wait_for_image_built(function: orchestrator.Function) -> None:
     container_image_tag = utils.container_image_tag_from_function(function)
-    if container_image_exists(container_image_tag):
-        logger.info("Found existing image", image=container_image_tag)
-    else:
-        attempt = 0
-        # with 60 attempts we wait max 2 min with a pending pod
-        max_attempts = MAX_IMAGE_BUILD_TIME / WAITING_TIME
+    attempt = 0
+    # with 60 attempts we wait max 2 min with a pending pod
+    max_attempts = MAX_IMAGE_BUILD_TIME / WAITING_TIME
+    while attempt < max_attempts:
+        if container_image_exists(container_image_tag):
+            logger.info("Found existing image", image=container_image_tag)
+            return
 
-        while attempt < max_attempts:
-            if container_image_exists:
-                logger.info("Found existing image", image=container_image_tag)
-            else:
-                attempt += 1
-                time.sleep(WAITING_TIME)
+        attempt += 1
+        time.sleep(WAITING_TIME)
 
-        raise exceptions.PodTimeoutError(
-            f"Build for function {function.key} didn't complete" f" after {MAX_IMAGE_BUILD_TIME} seconds"
-        )
+    raise exceptions.PodTimeoutError(
+        f"Build for function {function.key} didn't complete after {MAX_IMAGE_BUILD_TIME} seconds"
+    )
