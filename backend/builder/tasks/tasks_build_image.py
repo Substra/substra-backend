a3ecb533f398f1b574ea955996f5f58d817167f1
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-07-20 16:21:26 +0200

    feat: create `BuildTask`
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- backend/builder/tasks/tasks_build_image.py
+++ backend/builder/tasks/tasks_build_image.py
@@ -1,6 +1,7 @@
 import orchestrator
 from backend.celery import app
 from builder.image_builder.image_builder import build_image_if_missing
+from builder.tasks.task import BuildTask
 
 
 @app.task(
@@ -8,10 +9,11 @@ from builder.image_builder.image_builder import build_image_if_missing
     acks_late=True,
     reject_on_worker_lost=True,
     ignore_result=False,
+    base=BuildTask,
 )
 # Ack late and reject on worker lost allows use to
 # see http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-reject-on-worker-lost
 # and https://github.com/celery/celery/issues/5106
-def build_image(self, function_serialized: str, channel: str):
+def build_image(task: BuildTask, function_serialized: str, channel: str, _compute_task_key: str) -> None:
     function = orchestrator.Function.parse_raw(function_serialized)
     build_image_if_missing(channel, function)
