a82e85e77ac2f850f137e86f4e7536945c040981
Olivier Léobal <olivier.leobal@owkin.com>
2023-07-31 15:32:00 +0200

    chore!: rename the "detail" field in DRF exceptions to "message"
    Signed-off-by: Olivier Léobal <olivier.leobal@owkin.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,7 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
-- Increase the number of tasks displayable in frontend workflow [#697](https://github.com/Substra/substra-backend/pull/697)
+- Increase the number of tasks displayable in frontend workflow ([#697](https://github.com/Substra/substra-backend/pull/697))
+- BREAKING: Change the format of some error API responses from `{"detail": ...}` to `{"message": ...}`, to match the rest of the API ([#699](https://github.com/Substra/substra-backend/pull/699))
 
 ## [0.40.0](https://github.com/Substra/substra-backend/releases/tag/0.40.0) 2023-07-25
 
--- backend/api/views/exception_handler.py
+++ backend/api/views/exception_handler.py
@@ -22,4 +22,24 @@ def api_exception_handler(exc, context):
         logger.warning("exception", class_name=exc.__class__.__name__, exception=exc)
         return exc.response()
 
-    return views.exception_handler(exc, context)
+    return rename_detail_to_message(views.exception_handler(exc, context))
+
+
+def rename_detail_to_message(response):
+    """
+    rename DRF's "detail" field to "message", to match our own style
+    """
+
+    if not hasattr(response, "data") or "detail" not in response.data:
+        return response
+
+    if "message" not in response.data:
+        response.data["message"] = response.data["detail"]
+        del response.data["detail"]
+    else:
+        # not sure what to do in this case, let's just exchange them
+        detail = response.data["message"]
+        response.data["message"] = response.data["detail"]
+        response.data["detail"] = detail
+
+    return response
