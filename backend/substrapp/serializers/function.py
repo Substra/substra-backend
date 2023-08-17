f1f04a4d271d0f917189a6cf6ac0eb86adb86d4e
SdgJlbl <sarah.diot-girard@owkin.com>
2023-08-17 16:53:12 +0200

    feat: share images between backends (#708)
    Signed-off-by: SdgJlbl <sarah.diot-girard@owkin.com>

--- backend/substrapp/serializers/function.py
+++ backend/substrapp/serializers/function.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from libs.serializers import DynamicFieldsModelSerializer
 from substrapp.models import Function
+from substrapp.models import FunctionImage
 from substrapp.serializers.utils import FileSizeValidator
 from substrapp.serializers.utils import FileValidator
 
