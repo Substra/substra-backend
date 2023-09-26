36a34b3b1a93a9f6bfde0afa54a2ed44f87608b9
Thibault Camalon <135698225+thbcmlowk@users.noreply.github.com>
2023-09-26 16:38:28 +0200

    refactor(app/orchestrator/resources): FunctionStatus.FUNCTION_STATUS_CREATED -> FunctionStatus.FUNCTION_STATUS_WAITING
    Signed-off-by: Thibault Camalon <135698225+thbcmlowk@users.noreply.github.com>

--- backend/orchestrator/resources.py
+++ backend/orchestrator/resources.py
@@ -140,7 +140,7 @@ class FunctionOutput(pydantic.BaseModel):
 
 class FunctionStatus(AutoNameEnum):
     FUNCTION_STATUS_UNKNOWN = enum.auto()
-    FUNCTION_STATUS_CREATED = enum.auto()
+    FUNCTION_STATUS_WAITING = enum.auto()
     FUNCTION_STATUS_BUILDING = enum.auto()
     FUNCTION_STATUS_READY = enum.auto()
     FUNCTION_STATUS_CANCELED = enum.auto()
