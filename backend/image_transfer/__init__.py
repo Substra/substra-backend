b7ccaca604fd75967a5a7211d0d963ed49680b95
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-10-09 09:40:14 +0200

    chore: clean image transfer and add tests
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- backend/image_transfer/__init__.py
+++ backend/image_transfer/__init__.py
@@ -1,6 +1,7 @@
-from image_transfer.decoder import BlobNotFound
-from image_transfer.decoder import ManifestNotFound
+# The image transfer module is a copy of docker charon -> https://github.com/gabrieldemarmiesse/docker-charon
+# Some unused features have been remove and can be found in the original repository.
+
 from image_transfer.decoder import push_payload
 from image_transfer.encoder import make_payload
 
-__all__ = (BlobNotFound, ManifestNotFound, push_payload, make_payload)
+__all__ = (push_payload, make_payload)
