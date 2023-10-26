665c3d8b4eb45f09535cb17a97169971b27591ce
SdgJlbl <sarah.diot-girard@owkin.com>
2023-10-26 17:26:28 +0200

    fix: remove CRON image cleaning task
    Signed-off-by: SdgJlbl <sarah.diot-girard@owkin.com>

--- backend/substrapp/tasks/tasks_docker_registry.py
+++ backend/substrapp/tasks/tasks_docker_registry.py
@@ -1,14 +1,5 @@
 from backend.celery import app
-from substrapp.docker_registry import delete_container_image_safe
-from substrapp.docker_registry import fetch_old_function_image_names
 from substrapp.docker_registry import run_garbage_collector
-
-
-@app.task(ignore_result=True)
-def clean_old_images_task(max_duration: int) -> None:
-    function_image_names = fetch_old_function_image_names(max_duration)
-    for function_image_name in function_image_names:
-        delete_container_image_safe(function_image_name)
 
 
 @app.task(ignore_result=True)
--- backend/substrapp/tests/tasks/test_compute_plan.py
+++ backend/substrapp/tests/tasks/test_compute_plan.py
@@ -2,7 +2,6 @@ import uuid
 
 from pytest_mock import MockerFixture
 
-import orchestrator.mock as orc_mock
 from substrapp.tasks import tasks_compute_plan
 
 
@@ -10,7 +9,6 @@ def test_teardown_compute_plan_resources_cp_doing(mocker: MockerFixture):
     client = mocker.Mock()
     client.is_compute_plan_running.return_value = True
     mocked_teardown = mocker.patch("substrapp.tasks.tasks_compute_plan._teardown_pods_and_dirs")
-    mocked_function_delete = mocker.patch("substrapp.tasks.tasks_compute_plan._delete_compute_plan_functions_images")
 
     cp_key = str(uuid.uuid4())
 
@@ -18,14 +16,12 @@ def test_teardown_compute_plan_resources_cp_doing(mocker: MockerFixture):
 
     client.is_compute_plan_running.assert_called_once()
     mocked_teardown.assert_not_called()
-    mocked_function_delete.assert_not_called()
 
 
 def test_teardown_compute_plan_resources_cp_done(mocker: MockerFixture):
     client = mocker.Mock()
     client.is_compute_plan_running.return_value = False
     mocked_teardown = mocker.patch("substrapp.tasks.tasks_compute_plan._teardown_pods_and_dirs")
-    mocked_function_delete = mocker.patch("substrapp.tasks.tasks_compute_plan._delete_compute_plan_functions_images")
 
     cp_key = str(uuid.uuid4())
 
@@ -33,20 +29,3 @@ def test_teardown_compute_plan_resources_cp_done(mocker: MockerFixture):
 
     client.is_compute_plan_running.assert_called_once()
     mocked_teardown.assert_called_once()
-    mocked_function_delete.assert_called_once()
-
-
-def test_delete_cp_function_images(mocker: MockerFixture):
-    function_1_address = orc_mock.AddressFactory(checksum="azerty")
-    function_2_address = orc_mock.AddressFactory(checksum="qwerty")
-    functions = [
-        orc_mock.FunctionFactory(function_address=function_1_address),
-        orc_mock.FunctionFactory(function_address=function_2_address),
-    ]
-    mocked_delete_image = mocker.patch("substrapp.tasks.tasks_compute_plan.delete_container_image_safe")
-
-    tasks_compute_plan._delete_compute_plan_functions_images(functions)
-
-    mocked_delete_image.assert_any_call("function-azerty")
-    mocked_delete_image.assert_any_call("function-qwerty")
-    assert mocked_delete_image.call_count == 2
