ebaf4e1c4d39ff08e696415d469e65f1ab5c3d8c
Guilhem Barthés <guilhem.barthes@owkin.com>
2023-10-31 12:39:36 +0100

    wip: try `CELERY_WORKER_PREFETCH_MULTIPLIER`
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- backend/backend/settings/celery/dev.py
+++ backend/backend/settings/celery/dev.py
@@ -2,5 +2,3 @@ from ..deps.image_build import *
 from ..deps.ledger import *
 from ..deps.orchestrator import *
 from ..dev import *
-
-CELERYD_PREFETCH_MULTIPLIER = 16
--- backend/backend/settings/celery/prod.py
+++ backend/backend/settings/celery/prod.py
@@ -2,5 +2,3 @@ from ..deps.image_build import *
 from ..deps.ledger import *
 from ..deps.orchestrator import *
 from ..prod import *
-
-CELERYD_PREFETCH_MULTIPLIER = 32
