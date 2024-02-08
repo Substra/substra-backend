188d0565a41d372a0b4ba9b0f3e846fa71e808e5
ThibaultFy <thibault.fouqueray@gmail.com>
2024-02-08 14:10:11 +0100

    chore: use save image task output to update orc function
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- backend/substrapp/tasks/tasks_save_image.py
+++ backend/substrapp/tasks/tasks_save_image.py
@@ -51,30 +51,13 @@ class SaveImageTask(FailableTask):
         return function.key, channel_name
 
     # Celery does not provide unpacked arguments, we are doing it in `get_task_info`
-    def on_success(self, retval: dict[str, Any], task_id: str, args: tuple, kwargs: dict[str, Any]) -> None:
-        function_key, channel_name = self.get_task_info(args, kwargs)
-
-        # needed because the name is not an optional parameter
-        api_function = ApiFunction.objects.get(key=function_key)
-
-        function_image = FunctionImage.objects.get(function=api_function)
-
-        orc_function = {
-            "key": str(function_key),
-            # TODO find a way to propagate the name or make it optional at update
-            "name": api_function.name,
-            "image": {
-                "checksum": function_image.checksum,
-                # TODO check url
-                "storage_address": settings.DEFAULT_DOMAIN + reverse("api:function-image", args=[function_key]),
-            },
-        }
+    def on_success(self, retval: tuple[str, str], task_id: str, args: tuple, kwargs: dict[str, Any]) -> None:
+        orc_update_function_param, channel_name = retval
 
         with get_orchestrator_client(channel_name) as client:
-            # TODO atomiticy
-            client.update_function(orc_function)
+            client.update_function(orc_update_function_param)
             client.update_function_status(
-                function_key=function_key, action=orchestrator.function_pb2.FUNCTION_ACTION_READY
+                function_key=orc_update_function_param["key"], action=orchestrator.function_pb2.FUNCTION_ACTION_READY
             )
 
 
@@ -122,4 +105,15 @@ def save_image_task(task: SaveImageTask, function_serialized: str, channel_name:
 
         logger.info("Serialized image saved")
 
-    return function_serialized, channel_name
+        orc_update_function_param = {
+            "key": str(api_function.key),
+            # TODO find a way to propagate the name or make it optional at update
+            "name": api_function.name,
+            "image": {
+                "checksum": api_function.image_checksum,
+                # TODO check url
+                "storage_address": api_function.image_address,
+            },
+        }
+
+        return orc_update_function_param, channel_name
