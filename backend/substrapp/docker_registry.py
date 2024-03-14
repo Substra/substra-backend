1939cbf71aa220a8f200c836eadf2e61bb17c646
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-03-14 17:40:48 +0100

    fix: use `schemaVersion` version 2 to get entrypoint
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- backend/substrapp/docker_registry.py
+++ backend/substrapp/docker_registry.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import typing
 from pathlib import Path
@@ -50,6 +51,31 @@ def get_registry_auth() -> typing.Optional[DockerAuthDict]:
     return None
 
 
+def get_request_docker_api(
+    path: str, header_accept: str = "application/vnd.docker.distribution.manifest.v2+json"
+) -> dict:
+    headers = {"Accept": header_accept}
+
+    auth_content = get_registry_auth()
+
+    if auth_content:
+        username = auth_content["username"]
+        password = auth_content["password"]
+        headers["Authorization"] = "Basic " + base64.b64encode(f"{username}:{password}".encode("utf-8")).decode("utf-8")
+
+    response = requests.get(
+        f"{REGISTRY_SCHEME}://{REGISTRY}/v2/{path}",
+        headers=headers,
+        timeout=HTTP_CLIENT_TIMEOUT_SECONDS,
+        # TODO: add verification before release
+        verify=False,  # nosec B501
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
 def get_container_image_name(image_name: str) -> str:
     pull_domain = REGISTRY_PULL_DOMAIN
 
@@ -61,47 +87,25 @@ def get_container_image_name(image_name: str) -> str:
 
 
 def get_entrypoint(image_tag: str) -> str:
-    d = get_container_image(image_tag)
-    return json.loads(d["history"][0]["v1Compatibility"])["config"]["Entrypoint"]
+    manifest = get_container_manifest(image_tag)
+    blob = get_container_blob(manifest["config"]["digest"])
+
+    return blob["config"]["Entrypoint"]
 
 
-def get_container_image(image_name: str) -> dict:
-    response = requests.get(
-        f"{REGISTRY_SCHEME}://{REGISTRY}/v2/{USER_IMAGE_REPOSITORY}/manifests/{image_name}",
-        headers={"Accept": "application/json"},
-        timeout=HTTP_CLIENT_TIMEOUT_SECONDS,
-        # TODO: add verification before release
-        verify=False,  # nosec B501
+def get_container_manifest(image_name: str) -> dict:
+    try:
+        return get_request_docker_api(
+            f"{USER_IMAGE_REPOSITORY}/manifests/{image_name}",
+        )
+    except requests.exceptions.HTTPError as err:
+        raise ImageNotFoundError(f"Error when querying docker-registry, status code: {err.response.status_code}")
+
+
+def get_container_blob(digest: str) -> dict:
+    return get_request_docker_api(
+        f"{USER_IMAGE_REPOSITORY}/blobs/{digest}",
     )
-    if response.status_code != requests.status_codes.codes.ok:
-        raise ImageNotFoundError(f"Error when querying docker-registry, status code: {response.status_code}")
-
-    return response.json()
-
-
-def get_container_images() -> list[dict]:
-    response = requests.get(
-        f"{REGISTRY_SCHEME}://{REGISTRY}/v2/_catalog",
-        headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"},
-        timeout=HTTP_CLIENT_TIMEOUT_SECONDS,
-    )
-
-    response.raise_for_status()
-    res = response.json()
-
-    for repository in res["repositories"]:
-        # get only user-image repo, images built by substra-backend
-        if repository == USER_IMAGE_REPOSITORY:
-            response = requests.get(
-                f"{REGISTRY_SCHEME}://{REGISTRY}/v2/{repository}/tags/list",
-                headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"},
-                timeout=HTTP_CLIENT_TIMEOUT_SECONDS,
-            )
-
-            response.raise_for_status()
-            return response.json()
-
-    return None
 
 
 def run_garbage_collector() -> None:
