8acf3ef8879d2c6f47cc430877071387fbb31ab2
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-04-19 16:22:15 +1000

    feat: add internet communication for builder
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- backend/substrapp/compute_tasks/compute_pod.py
+++ backend/substrapp/compute_tasks/compute_pod.py
@@ -23,7 +23,7 @@ class Label:
     FunctionKey = "substra.ai/function-key"
     RandomToken = "substra.ai/random-token"
     RoleRegistryClient = "role-registry-client"
-
+    RoleInternetEgress = "role-internet-egress"
     # Values
     PodType_ComputeTask = "compute-task"
     Component_Compute = "substra-compute"
