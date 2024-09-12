40cc00a9407c004032454bc4c4f1d646368c69ec
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-09-12 11:11:23 +0200

    chore: remove unused init
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- backend/organization/management/commands/sync_incoming_organization.py
+++ backend/organization/management/commands/sync_incoming_organization.py
@@ -5,9 +5,6 @@ from .base_sync import BaseSyncCommand
 
 class Command(BaseSyncCommand):
     help = "Sync incoming organizations"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.model = IncomingOrganization
-        self.model_name = "Incoming organization"
-        self.field_key = "organization_id"
+    model = IncomingOrganization
+    model_name = "Incoming organization"
+    field_key = "organization_id"
