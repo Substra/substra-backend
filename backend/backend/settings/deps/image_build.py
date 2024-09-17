513842f244b6f152ac4e307741c620f612036c0e
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-09-17 16:39:07 +0200

    feat: make image builder timeout configurable (#992)
    * chore: change default value for `IMAGE_BUILD_TIMEOUT`
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * feat: add `builder.timeout` to change `IMAGE_BUILD_TIMEOUT`
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * feat: add test for `substrap.lock_local.lock_resource`
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * chore: remove unused `IMAGE_BUILD_TIMEOUT`
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * fix: replace `Exception` by `LockError` as `Exception` does not accept custom named arguments
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * chore: update chart changelog
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * doc: update chart changelog
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    * doc: change fragment
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>
    
    ---------
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- backend/backend/settings/deps/image_build.py
+++ backend/backend/settings/deps/image_build.py
@@ -1,7 +1,7 @@
 import os
 
 # How long we wait before throwing errors, in seconds
-IMAGE_BUILD_TIMEOUT = 3 * 60 * 60  # 3 hours
+IMAGE_BUILD_TIMEOUT = int(os.getenv("IMAGE_BUILD_TIMEOUT", 60 * 60))  # 1 hour
 # Delay before two check
 IMAGE_BUILD_CHECK_DELAY = 5
 BUILDER_KANIKO_STARTUP_MAX_ATTEMPTS = int(os.getenv("BUILDER_KANIKO_STARTUP_MAX_ATTEMPTS", 60))
---
+++ backend/substrapp/tests/test_lock_local.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+from substrapp import lock_local
+from substrapp.exceptions import LockError
+
+
+# Test that `lock_local.lock_resource() returns the correct excception
+def test_lock_local_timeout():
+    resource_type = "test"
+    unique_identifier = "1"
+    # Create fake lock
+    slug = f"{resource_type}_{unique_identifier}"
+    lock_path = Path(lock_local._get_lock_file_path(slug))
+    lock_path.write_text("unique_id")
+
+    with pytest.raises(LockError):
+        with lock_local.lock_resource(resource_type, unique_identifier, timeout=1):
+            pass
